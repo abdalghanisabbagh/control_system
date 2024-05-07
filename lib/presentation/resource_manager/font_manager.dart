aed8b97fe9a59afc217aa2e3308dda62c328f3cb
abdalghanisabbagh <92229524+abdalghanisabbagh@users.noreply.github.com>
2024-05-07 12:51:45 +0300

    Add color, font, style, and theme managers
    Added color, font, style, and theme managers to manage colors, fonts, text styles, and themes in the application. This will help maintain consistency and ease of theming across the app.

---
+++ lib/presentation/resource_manager/font_manager.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+class FontConstant {
+  static const String fontFamilyName = '';
+}
+
+class FontWeightManager {
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight reguler = FontWeight.w400;
+  static const FontWeight meduim = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+}
+
+class FontSize {
+  static const double s10 = 10;
+  static const double s12 = 12;
+  static const double s14 = 14;
+  static const double s16 = 16;
+  static const double s18 = 18;
+  static const double s20 = 20;
+  static const double s24 = 24;
+  static const double s26 = 26;
+  static const double s30 = 30;
+  static const double s60 = 60;
+}
