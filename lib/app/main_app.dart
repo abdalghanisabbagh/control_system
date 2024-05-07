549c09d270333e3b33600657d61a0d4ef42a3933
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-07 11:52:42 +0300

    ✨ Add Get package to improve state management in the app
    - Added Get package to manage state more efficiently
    - Updated MainApp to GetMaterialApp for state management
    - Created HomeScreen with GetView for better state handling
    - Added HomeBindings to bind HomeController for improved state management

--- lib/app/main_app.dart
+++ lib/app/main_app.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
     );
   }
 }
---
+++ lib/presentation/home/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:control_system/domain/controllers/home_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/state_manager.dart';
+
+class HomeScreen extends GetView<HomeController> {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeScreenScreen'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'HomeScreenScreen is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
