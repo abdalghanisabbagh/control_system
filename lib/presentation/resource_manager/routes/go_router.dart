2447cf86b0eb2ae5f428543b36a8bd10534e3e2b
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-07 12:08:38 +0300

    ✨ Export required files from home_index
    Export the home_bindings.dart and home_controller.dart files from the home_index.dart file in the domain/indices directory.

--- lib/presentation/resource_manager/routes/go_router.dart
+++ lib/presentation/resource_manager/routes/go_router.dart
@@ -1,9 +1,8 @@
-import 'package:control_system/domain/bindings/home_bindings.dart';
 import 'package:control_system/presentation/resource_manager/routes/app_routes_names_and_paths.dart';
 import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
 
-import '../../../domain/controllers/home_controller.dart';
+import '../../../domain/indices/home_index.dart';
 import '../../views/home/home_screen.dart';
 
 class AppGoRouter {
---
+++ lib/presentation/resource_manager/routes/index.dart
@@ -0,0 +1,2 @@
+export 'app_routes_names_and_paths.dart';
+export 'go_router.dart';
