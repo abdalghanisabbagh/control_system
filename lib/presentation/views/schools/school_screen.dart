cb3c10e208548e4fc1f9096656c988681a5e2ab8
Abdelrahman Elzakazeky <42267852+Azakazeky@users.noreply.github.com>
2024-05-13 12:54:37 +0300

    school screen

---
+++ lib/presentation/views/schools/school_screen.dart
@@ -0,0 +1,16 @@
+import 'package:control_system/presentation/views/base_screen.dart';
+import 'package:flutter/material.dart';
+
+class SchoolsScreen extends StatelessWidget {
+  const SchoolsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseScreen(
+      key: key,
+      body: Center(
+        child: Text("Schools Sreen"),
+      ),
+    );
+  }
+}
