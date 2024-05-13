dfc7a9060a5f304ed697f9323afb6aec80754824
abdalghani sabbagh <92229524+abdalghanisabbagh@users.noreply.github.com>
2024-05-13 12:48:33 +0300

    Add new side menu items and update logo in HomeScreen
    Added new side menu items with icons and tooltips for better navigation
    experience. Also updated the logo in the side menu title for consistency
    with the branding.

--- lib/presentation/views/home/home_screen.dart
+++ lib/presentation/views/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:control_system/domain/controllers/home_controller.dart';
 import 'package:control_system/presentation/resource_manager/assets_manager.dart';
+import 'package:control_system/presentation/resource_manager/index.dart';
 import 'package:control_system/presentation/views/home/widgets/home_screen_test.dart';
 import 'package:easy_sidemenu/easy_sidemenu.dart';
 import 'package:flutter/material.dart';
@@ -13,30 +14,115 @@ class HomeScreen extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('title'),
-        centerTitle: true,
-      ),
       body: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SideMenu(
+            
+            showToggle: true,
             items: [
               SideMenuItem(
                 title: 'Dashboard',
                 onTap: (index, _) {
                   controller.sideMenu.changePage(index);
                 },
-                icon: const Icon(Icons.home),
+                iconWidget: Image.asset(AssetsManager.assetsIconsDashboard),
                 tooltipContent: "This is a tooltip for Dashboard item",
               ),
               SideMenuItem(
-                title: 'test',
+                title: 'Students',
                 onTap: (index, _) {
-                  //sideMenu.changePage(index);
                   controller.sideMenu.changePage(index);
                 },
-                icon: const Icon(Icons.ac_unit_rounded),
+                iconWidget: Image.asset(AssetsManager.assetsIconsStudent),
+                tooltipContent: "This is a tooltip for Dashboard item",
+              ),
+              SideMenuItem(
+                title: 'Control Batch',
+                onTap: (index, _) {
+                  controller.sideMenu.changePage(index);
+                },
+                iconWidget: Image.asset(AssetsManager.assetsIconsExam),
+                tooltipContent: "This is a tooltip for Dashboard item",
+              ),
+              SideMenuItem(
+                title: 'Batch Documents',
+                onTap: (index, _) {
+                  controller.sideMenu.changePage(index);
+                },
+                iconWidget: Image.asset(AssetsManager.assetsIconsPatchDoc),
+                tooltipContent: "This is a tooltip for Dashboard item",
+              ),
+              SideMenuItem(
+                title: 'Certificates',
+                onTap: (index, _) {
+                  controller.sideMenu.changePage(index);
+                },
+                iconWidget: Image.asset(AssetsManager.assetsIconsCertificates),
+                tooltipContent: "This is a tooltip for Dashboard item",
+              ),
+              SideMenuItem(
+                title: 'Scanning Grades',
+                onTap: (index, _) {
+                  controller.sideMenu.changePage(index);
+                },
+                iconWidget: Image.asset(AssetsManager.assetsIconsProccess),
+                tooltipContent: "This is a tooltip for Dashboard item",
+              ),
+              SideMenuItem(
+                title: 'School',
+                onTap: (index, _) {
+                  controller.sideMenu.changePage(index);
+                },
+                iconWidget: Image.asset(AssetsManager.assetsIconsCampus),
+                tooltipContent: "This is a tooltip for Dashboard item",
+              ),
+              SideMenuItem(
+                title: 'Classrooms',
+                onTap: (index, _) {
+                  controller.sideMenu.changePage(index);
+                },
+                iconWidget: Image.asset(AssetsManager.assetsIconsClassRooms),
+                tooltipContent: "This is a tooltip for Dashboard item",
+              ),
+              SideMenuItem(
+                title: 'Subject',
+                onTap: (index, _) {
+                  controller.sideMenu.changePage(index);
+                },
+                iconWidget: Image.asset(AssetsManager.assetsIconsSupject),
+                tooltipContent: "This is a tooltip for Dashboard item",
+              ),
+              SideMenuItem(
+                title: 'Chorts',
+                onTap: (index, _) {
+                  controller.sideMenu.changePage(index);
+                },
+                iconWidget: Image.asset(AssetsManager.assetsIconsCohort),
+                tooltipContent: "This is a tooltip for Dashboard item",
+              ),
+              SideMenuItem(
+                title: 'Admins',
+                onTap: (index, _) {
+                  controller.sideMenu.changePage(index);
+                },
+                iconWidget: Image.asset(AssetsManager.assetsIconsAdmin),
+                tooltipContent: "This is a tooltip for Dashboard item",
+              ),
+              SideMenuItem(
+                title: 'Proctors',
+                onTap: (index, _) {
+                  controller.sideMenu.changePage(index);
+                },
+                iconWidget: Image.asset(AssetsManager.assetsIconsPatchDoc),
+                tooltipContent: "This is a tooltip for Dashboard item",
+              ),
+              SideMenuItem(
+                title: 'Sign Out',
+                onTap: (index, _) {
+                  controller.sideMenu.changePage(index);
+                },
+                iconWidget: Image.asset(AssetsManager.assetsIconsLogout),
                 tooltipContent: "This is a tooltip for Dashboard item",
               ),
             ],
@@ -51,14 +137,18 @@ class HomeScreen extends GetView<HomeController> {
               selectedTitleTextStyle: const TextStyle(color: Colors.white),
               selectedIconColor: Colors.white,
             ),
-            title: Column(
+            title: Row(
               children: [
                 ConstrainedBox(
                   constraints: const BoxConstraints(
-                    maxHeight: 150,
-                    maxWidth: 150,
+                    maxHeight: 100,
+                    maxWidth: 100,
                   ),
-                  child: Image.asset(AssetsManager.assetsIconsAdmin),
+                  child: Image.asset(AssetsManager.assetsLogosNisLogo22),
+                ),
+                Text(
+                  "Control System",
+                  style: TextStyle(fontSize: AppSize.s25),
                 ),
                 const Divider(
                   indent: 8.0,
