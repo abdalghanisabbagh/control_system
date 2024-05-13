f947471a7551149502b8458eb20fe5b3e3520d55
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-13 18:33:03 +0300

    🔧 Ignore constant identifier names in dio_factory

--- lib/Data/Network/tools/dio_factry.dart
+++ lib/Data/Network/tools/dio_factry.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:control_system/app/configurations/app_links.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
