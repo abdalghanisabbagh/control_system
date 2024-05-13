2022c3376c5425313e24eb2fe9f1a43df9489885
Abdelrahman Elzakazeky <42267852+Azakazeky@users.noreply.github.com>
2024-05-13 13:14:10 +0300

    add network tools

--- lib/domain/controllers/auth_controller.dart
+++ lib/domain/controllers/auth_controller.dart
@@ -1,9 +1,20 @@
+import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
-class AuthController extends GetxController{
-  
+class AuthController extends GetxController {
   RxBool isLogin = false.obs;
   RxBool isLoading = false.obs;
-  List schools=[];
-  
+  List schools = [];
+
+  Future login(String username, String password) async {
+    isLoading.value = true;
+
+    var response = await Dio().post('http://localhost:3333/auth/login', data: {
+      "username": username,
+      "password": password,
+    });
+
+    isLoading.value = false;
+    isLogin.value = true;
+  }
 }
