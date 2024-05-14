c36b1331cc101b8e1223e28d05025437e4028c7f
abdalghani sabbagh <92229524+abdalghanisabbagh@users.noreply.github.com>
2024-05-14 10:31:12 +0300

    Add new API endpoint links
    Added new API endpoint links for authentication, schools, students, education years,
    control missions, exams, and user roles systems. This update includes the addition of
    various specific endpoint URLs for different functionalities.

--- lib/app/configurations/app_links.dart
+++ lib/app/configurations/app_links.dart
@@ -4,4 +4,59 @@ class AppLinks {
 
 class AuthLinks {
   static const login = 'auth/login';
+  static const refresh = 'auth/refresh';
+  static const logout = 'auth/logout';
+  static const user = 'users';
+  static const userAddRoles = 'users/add-roles';
+  static const usersAddSchools = 'users/add-schools';
+}
+
+class SchoolsLinks {
+  static const schools = 'schools';
+  static const grades = 'grades';
+  static const gradesSchools = 'grades/schools';
+  static const cohort = 'cohort';
+  static const subject = 'subject';
+  static const schoolsClasses = 'school-classes';
+  static const schoolsType = 'school-type';
+}
+
+class StudentsLinks {
+  static const student = 'student';
+  static const studentsClass = 'student/class';
+  static const studentCohort = 'student/cohort';
+  static const studentSchool = 'student/school';
+  static const studentBarcodes = 'student-barcodes';
+  static const studentBarcodesStudent = 'student-barcodes/student';
+  static const studentBarcodesExamMission = 'student-barcodes/exam-mission';
+  static const studentSetNumbers = 'student-set-numbers';
+  static const studentSetNumbersControlMission =
+      'student-set-numbers/control-mission';
+  static const studentSetNumbersStudent = 'student-set-numbers/student';
+  static const studentSetNumbersExamRoom = 'student-set-numbers/exam-rooms';
+}
+
+class EducationYearsLinks {
+  static const educationyear = 'education-year';
+}
+
+class ControlMissionLinks {
+  static const controlMission = 'control-mission';
+  static const controlMissionSchool = 'control-mission/school';
+  static const controlMissionEducationYear = 'control-mission/education-year';
+}
+
+class ExamLinks {
+  static const examMission = 'exam-mission';
+  static const examMissionSubject = 'exam-mission/subject';
+  static const examMissionControlMission = 'exam-mission/control-mission';
+  static const examRooms = 'exam-rooms';
+  static const examRoomsControlMission = 'exam-rooms/control-mission';
+  static const examRoomsSchoolClass = 'exam-rooms/school-class';
+}
+
+class UserRolesSystemsLink {
+  static const userRolesSystems = 'user-roles-systems';
+  static const userRolesSystemsConnectRolesTOScreens =
+      'user-roles-systems/connect-roles-to-screens';
 }
