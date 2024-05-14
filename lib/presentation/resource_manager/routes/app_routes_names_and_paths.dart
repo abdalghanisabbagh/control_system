af14a4aa8171565facdc9445fc24cdb30c20db2e
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-14 07:51:47 +0300

    ✨ Add new paths and names in AppRoutesNamesAndPaths
    Added new screens with their corresponding paths and names to the AppRoutesNamesAndPaths class. This will help in better organization and navigation within the application.

--- lib/presentation/resource_manager/routes/app_routes_names_and_paths.dart
+++ lib/presentation/resource_manager/routes/app_routes_names_and_paths.dart
@@ -10,4 +10,48 @@ class AppRoutesNamesAndPaths {
   // schoolsScreen
   static const String schoolsScreenPath = '/schools';
   static const String schoolsScreenName = 'schools';
+
+  // certificateScreen
+  static const String certificateScreenPath = '/certificate';
+  static const String certificateScreenName = 'certificate';
+
+  // classRoomScreen
+  static const String classRoomScreenPath = '/class-room';
+  static const String classRoomScreenName = 'class-room';
+
+  // cohortSettingScreen
+  static const String cohortSettingScreenPath = '/cohort-setting';
+  static const String cohortSettingScreenName = 'cohort-setting';
+
+  // controlMissionScreen
+  static const String controlMissionScreenPath = '/control-mission';
+  static const String controlMissionScreenName = 'control-mission';
+
+  // dashBoardScreen
+  static const String dashBoardScreenPath = '/dashboard';
+  static const String dashBoardScreenName = 'dashboard';
+
+  // proctorScreen
+  static const String proctorScreenPath = '/proctor';
+  static const String proctorScreenName = 'proctor';
+
+  // seatingNumbersScreen
+  static const String seatingNumbersScreenPath = '/seating-numbers';
+  static const String seatingNumbersScreenName = 'seating-numbers';
+
+  // setDegreesScreen
+  static const String setDegreesScreenPath = '/set-degrees';
+  static const String setDegreesScreenName = 'set-degrees';
+
+  // studentScreen
+  static const String studentScreenPath = '/student';
+  static const String studentScreenName = 'student';
+
+  // subjectSettingScreen
+  static const String subjectSettingScreenPath = '/subject-setting';
+  static const String subjectSettingScreenName = 'subject-setting';
+
+  // userScreen
+  static const String userScreenPath = '/user';
+  static const String userScreenName = 'user';
 }
