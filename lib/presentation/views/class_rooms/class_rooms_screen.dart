9fa5f00b00a4220759ec22b8bbebbf3084d346f9
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-15 10:37:01 +0300

    ✨ Add reusable header widget to Class Rooms Screen
    Imported and added HeaderWidget to display title in Class Rooms screen.
    Updated layout with background color, padding, and Add Class Room button.
    Ensuring consistent design and user experience across screens.

--- lib/presentation/views/class_rooms/class_rooms_screen.dart
+++ lib/presentation/views/class_rooms/class_rooms_screen.dart
@@ -1,5 +1,8 @@
+import 'package:control_system/presentation/resource_manager/index.dart';
 import 'package:control_system/presentation/views/base_screen.dart';
 import 'package:flutter/material.dart';
+
+import '../../resource_manager/ReusableWidget/header_widget.dart';
 
 class ClassRoomsScreen extends StatelessWidget {
   const ClassRoomsScreen({super.key});
@@ -7,9 +10,40 @@ class ClassRoomsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BaseScreen(
-      key: key,
-      body: const Center(
-        child: Text("Class Rooms Screen"),
+      body: Container(
+        color: ColorManager.bgColor,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const Expanded(child: HeaderWidget(text: "Class Rooms")),
+                InkWell(
+                  onTap: () {
+                    // Get.toNamed(Routes.classRoomSeats);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: ColorManager.glodenColor,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          "Add Class Room",
+                          style: nunitoBold.copyWith(
+                            color: ColorManager.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
