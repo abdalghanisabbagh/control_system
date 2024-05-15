01dea32adf5f01571e68f256e273ebadda1abc3f
abdalghani sabbagh <92229524+abdalghanisabbagh@users.noreply.github.com>
2024-05-15 10:15:36 +0300

    Add new proctor and edit proctor and edit student widgets
    Add new proctor, edit proctor, and edit student widgets to the project. Each widget
    contains a placeholder widget for now.

---
+++ lib/presentation/views/proctor/widgets/edit_proctor.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditProctor extends StatelessWidget {
+  const EditProctor({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
