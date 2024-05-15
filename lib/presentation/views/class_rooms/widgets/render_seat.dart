fd2edc7c70064157e047d9527c0f3216dfc5786f
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-15 10:55:56 +0300

    ✨ Add render seat screen route and widget
    Added a new route for rendering seat screen in the app with corresponding widget.

---
+++ lib/presentation/views/class_rooms/widgets/render_seat.dart
@@ -0,0 +1,11 @@
+import 'package:control_system/presentation/views/base_screen.dart';
+import 'package:flutter/material.dart';
+
+class RenderSeats extends StatelessWidget {
+  const RenderSeats({super.key});
+  final int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return BaseScreen(body: Container());
+  }
+}
