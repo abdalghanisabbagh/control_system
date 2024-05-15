7841ca69b66ceae785771f97eec423f77f7c8485
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-15 10:29:16 +0300

    ✨ Add header widget component
    Added a new HeaderWidget component to display a header with custom text
    and styling.

---
+++ lib/presentation/resource_manager/ReusableWidget/header_widget.dart
@@ -0,0 +1,24 @@
+import 'package:control_system/presentation/resource_manager/index.dart';
+import 'package:flutter/material.dart';
+
+class HeaderWidget extends StatelessWidget {
+  final String text;
+  const HeaderWidget({super.key, required this.text});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Row(
+        children: [
+          Text(
+            text,
+            style: nunitoBlack.copyWith(
+              color: ColorManager.bgSideMenu,
+              fontSize: 30,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
