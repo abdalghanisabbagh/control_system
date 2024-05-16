3168ed5c51083fbeadbc833a5c09cf89a3f72e5e
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-16 12:17:30 +0300

    ✨ Update student screen layout
    - Added header student widget to the screen
    - Improved styling for the container
    - Removed commented out code
    - Added spacing between elements

--- lib/presentation/views/student/widgets/header_student_widget.dart
+++ lib/presentation/views/student/widgets/header_student_widget.dart
@@ -3,72 +3,68 @@ import 'package:control_system/presentation/resource_manager/index.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import '../../base_screen.dart';
-
 class HeaderStudentWidget extends StatelessWidget {
   const HeaderStudentWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BaseScreen(
-      body: Container(
-        margin: const EdgeInsets.all(10),
-        child: Row(
-          children: [
-            Text(
-              'Students',
-              style: nunitoBlack.copyWith(
-                color: ColorManager.bgSideMenu,
-                fontSize: 30,
-              ),
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Row(
+        children: [
+          Text(
+            'Students',
+            style: nunitoBlack.copyWith(
+              color: ColorManager.bgSideMenu,
+              fontSize: 30,
             ),
-            if (context.isDesktop) ...{
-              const Spacer(),
-              IconButton(
-                tooltip: "Promot Students From Excel",
-                icon: const Icon(FontAwesomeIcons.arrowUpFromGroundWater),
-                onPressed: () {
-                  // controller.promotFromExcel();
-                },
-              ),
-              IconButton(
-                tooltip: "Download excel template",
-                icon: const Icon(FontAwesomeIcons.cloudArrowDown),
-                onPressed: () {
-                  // controller.downloadeTemp();
-                },
-              ),
-              IconButton(
-                tooltip: "Import From Excel",
-                icon: const Icon(FontAwesomeIcons.fileExcel),
-                onPressed: () {
-                  // controller.importExcel();
-                },
-              ),
-              IconButton(
-                tooltip: "Add New Student",
-                icon: const Icon(FontAwesomeIcons.userPlus),
-                onPressed: () {
-                  // MyDialogs.showAddDialog(context, AddSinglStudent());
-                },
-              ),
-              IconButton(
-                tooltip: "Sync Students",
-                icon: const Icon(FontAwesomeIcons.rotate),
-                onPressed: () {
-                  // controller.getStudenFromServer();
-                },
-              ),
-              IconButton(
-                tooltip: "Send To DataBase",
-                icon: const Icon(Icons.send),
-                onPressed: () {
-                  // controller.sendStudenToServer(controller.students);
-                },
-              ),
-            }
-          ],
-        ),
+          ),
+          if (context.isDesktop) ...{
+            const Spacer(),
+            IconButton(
+              tooltip: "Promot Students From Excel",
+              icon: const Icon(FontAwesomeIcons.arrowUpFromGroundWater),
+              onPressed: () {
+                // controller.promotFromExcel();
+              },
+            ),
+            IconButton(
+              tooltip: "Download excel template",
+              icon: const Icon(FontAwesomeIcons.cloudArrowDown),
+              onPressed: () {
+                // controller.downloadeTemp();
+              },
+            ),
+            IconButton(
+              tooltip: "Import From Excel",
+              icon: const Icon(FontAwesomeIcons.fileExcel),
+              onPressed: () {
+                // controller.importExcel();
+              },
+            ),
+            IconButton(
+              tooltip: "Add New Student",
+              icon: const Icon(FontAwesomeIcons.userPlus),
+              onPressed: () {
+                // MyDialogs.showAddDialog(context, AddSinglStudent());
+              },
+            ),
+            IconButton(
+              tooltip: "Sync Students",
+              icon: const Icon(FontAwesomeIcons.rotate),
+              onPressed: () {
+                // controller.getStudenFromServer();
+              },
+            ),
+            IconButton(
+              tooltip: "Send To DataBase",
+              icon: const Icon(Icons.send),
+              onPressed: () {
+                // controller.sendStudenToServer(controller.students);
+              },
+            ),
+          }
+        ],
       ),
     );
   }
