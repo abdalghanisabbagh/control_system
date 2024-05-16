89b701b2d218065e2f816f8f48702aa34a96cedd
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-16 11:31:34 +0300

    ✨ Refactor the student screen header widget
    Added a separate header widget for the student screen to improve code
    organization and maintainability. Removed unnecessary icons and tooltips
    from the student screen widget to simplify the UI.

--- lib/presentation/views/student/student_screen.dart
+++ lib/presentation/views/student/student_screen.dart
@@ -1,65 +1,10 @@
-import 'package:control_system/app/extensions/device_type_extension.dart';
 import 'package:control_system/presentation/views/base_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class StudentScreen extends StatelessWidget {
   const StudentScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return BaseScreen(
-      body: Container(
-        margin: const EdgeInsets.all(10),
-        child: Row(
-          children: [
-            if (context.isDesktop) ...{
-              const Spacer(),
-              IconButton(
-                tooltip: "Promot Students From Excel",
-                icon: const Icon(FontAwesomeIcons.arrowUpFromGroundWater),
-                onPressed: () {
-                  // controller.promotFromExcel();
-                },
-              ),
-              IconButton(
-                tooltip: "Download excel template",
-                icon: const Icon(FontAwesomeIcons.cloudArrowDown),
-                onPressed: () {
-                  // controller.downloadeTemp();
-                },
-              ),
-              IconButton(
-                tooltip: "Import From Excel",
-                icon: const Icon(FontAwesomeIcons.fileExcel),
-                onPressed: () {
-                  // controller.importExcel();
-                },
-              ),
-              IconButton(
-                tooltip: "Add New Student",
-                icon: const Icon(FontAwesomeIcons.userPlus),
-                onPressed: () {
-                  // MyDialogs.showAddDialog(context, AddSinglStudent());
-                },
-              ),
-              IconButton(
-                tooltip: "Sync Students",
-                icon: const Icon(FontAwesomeIcons.rotate),
-                onPressed: () {
-                  // controller.getStudenFromServer();
-                },
-              ),
-              IconButton(
-                tooltip: "Send To DataBase",
-                icon: const Icon(Icons.send),
-                onPressed: () {
-                  // controller.sendStudenToServer(controller.students);
-                },
-              ),
-            }
-          ],
-        ),
-      ),
-    );
+    return BaseScreen(body: Container());
   }
 }
