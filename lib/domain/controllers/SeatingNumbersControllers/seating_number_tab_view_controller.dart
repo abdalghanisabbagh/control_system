0e9e34bf83ecb175dab50210db79856044cdaed4
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-19 10:33:14 +0300

    ✨ Refactor: Update SeatingNumbersScreen to BatchDocumentsScreen
    - Update routes in AppGoRouter for SeatingNumbersScreen to BatchDocumentsScreen
    - Rename SeatingNumberController to BatchDocumentsController
    - Modify Bindings for SeatingNumbers to BatchDocuments
    - Update SideMenueGetController to navigate to BatchDocumentsScreen
    - Remove SeatingNumbersScreen import in index.dart
    - Add CoverSheetsScreen widget for managing cover sheets

--- lib/domain/controllers/SeatingNumbersControllers/seating_number_tab_view_controller.dart
+++ lib/domain/controllers/SeatingNumbersControllers/seating_number_tab_view_controller.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SeatingNumberController extends GetxController {
+class BatchDocumentsController extends GetxController {
   TabController? tabController;
 }
--- lib/presentation/views/batch_documents/batch_documents_screen.dart
+++ lib/presentation/views/batch_documents/batch_documents_screen.dart
@@ -1,16 +1,63 @@
+import 'package:control_system/domain/controllers/SeatingNumbersControllers/seating_number_tab_view_controller.dart';
+import 'package:control_system/presentation/resource_manager/color_manager.dart';
 import 'package:control_system/presentation/views/base_screen.dart';
+import 'package:control_system/presentation/views/batch_documents/widgets/attendance_screen.dart';
+import 'package:control_system/presentation/views/batch_documents/widgets/cover_sheet_screen.dart';
+import 'package:control_system/presentation/views/batch_documents/widgets/seating_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class BatchDocumentsScreen extends StatelessWidget {
+import '../../resource_manager/styles_manager.dart';
+
+class BatchDocumentsScreen extends GetView<BatchDocumentsController> {
   const BatchDocumentsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BaseScreen(
-      key: key,
-      body: const Center(
-        child: Text("Batch Documents Screen"),
-      ),
-    );
+        key: key,
+        body: Container(
+          color: ColorManager.ligthBlue,
+          padding: const EdgeInsets.all(20),
+          child: GetBuilder<BatchDocumentsController>(
+            builder: (controller) => DefaultTabController(
+              length: 3,
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: ColorManager.primary),
+                    child: TabBar(
+                        overlayColor:
+                            MaterialStateProperty.all(ColorManager.primary),
+                        labelStyle: nunitoLightStyle().copyWith(fontSize: 30),
+                        automaticIndicatorColorAdjustment: true,
+                        labelColor: ColorManager.primary,
+                        labelPadding: const EdgeInsets.symmetric(vertical: 15),
+                        indicator: BoxDecoration(color: ColorManager.white),
+                        indicatorSize: TabBarIndicatorSize.tab,
+                        unselectedLabelColor: ColorManager.white,
+                        controller: controller.tabController,
+                        tabs: const [
+                          Text("Cover Sheet"),
+                          Text("Seat numbers"),
+                          Text("Attendance"),
+                        ]),
+                  ),
+                  Expanded(
+                    child: TabBarView(
+                        controller: controller.tabController,
+                        children: const [
+                          CoverSheetsScreen(),
+                          SeatingScreen(),
+                          AttendanceScreen()
+                        ]),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
