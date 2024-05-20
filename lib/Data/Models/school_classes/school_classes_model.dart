c919bde507ed570840399039b39b9efcf5985fcf
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-20 14:01:30 +0300

    ✨ Add SchoolClassesModel with basic attributes
    Added the SchoolClassesModel class with basic attributes such as name, maxCapacity,
    floor, rows, columns, and schoolsID. Includes toJson and fromJson methods for
    serialization/deserialization.

---
+++ lib/Data/Models/school_classes/school_classes_model.dart
@@ -0,0 +1,39 @@
+import 'package:control_system/Data/Models/base_model.dart';
+
+class SchoolClassesModel extends BaseModel {
+  String? name;
+  String? maxCapacity;
+  String? floor;
+  String? rows;
+  int? columns;
+  int? schoolsID;
+
+  SchoolClassesModel({
+    this.name,
+    this.maxCapacity,
+    this.floor,
+    this.rows,
+    this.columns,
+    this.schoolsID,
+  });
+
+  SchoolClassesModel.fromJson(Map<String, dynamic> json) {
+    name = json['Name'];
+    maxCapacity = json['Max_Capacity'];
+    floor = json['Floor'];
+    rows = json['Rows'];
+    columns = json['Columns'];
+    schoolsID = json['Schools_ID'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['Name'] = name;
+    data['Max_Capacity'] = maxCapacity;
+    data['Floor'] = floor;
+    data['Rows'] = rows;
+    data['Columns'] = columns;
+    data['Schools_ID'] = schoolsID;
+    return data;
+  }
+}
