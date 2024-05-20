064da23e6c6d172969dd58fa1399b790ed2f6829
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-20 12:37:53 +0300

    ✨ Update ResponseHandler to handle different response types
    Refactor ResponseHandler to take generic types to handle different response
    data. Now, it can handle different response data types based on the
    implementation.

--- lib/Data/Network/response_handler.dart
+++ lib/Data/Network/response_handler.dart
@@ -4,12 +4,14 @@ import 'package:dio/dio.dart';
 import '../handlers/interfaces/response_interface.dart';
 import 'tools/failure_model.dart';
 
-class ResponseHandler<T extends ResponseInterface> {
-  T? data;
+class ResponseHandler<T extends ResponseInterface, R> {
+  T data;
 
-  Either<Failure, T> getResponse(Response<dynamic> response) {
-    if (response.statusCode == 200) {
-      return Right(data!.fromJson(response.data));
+  ResponseHandler({required this.data});
+
+  Either<Failure, R> getResponse(Response<dynamic> response) {
+    if (response.data['status'] == true) {
+      return Right(data.fromJson(response.data['data']));
     } else {
       return Left(Failure(response.statusCode ?? 0, response.data['message']));
     }
