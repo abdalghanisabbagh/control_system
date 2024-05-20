fddb0a9cbcce5aa16cc5b5e7a96414fda46cd5de
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-20 14:08:45 +0300

    ✨ Add Control Mission Model class
    Added the ControlMessionModel class that extends BaseModel, includes properties
    educationYearID, schoolsID, and name, toJson and fromJson methods.

---
+++ lib/Data/Models/control_mission/control_mession_model.dart
@@ -0,0 +1,23 @@
+import 'package:control_system/Data/Models/base_model.dart';
+
+class ControlMessionModel extends BaseModel {
+  int? educationYearID;
+  int? schoolsID;
+  String? name;
+
+  ControlMessionModel({this.educationYearID, this.schoolsID, this.name});
+
+  ControlMessionModel.fromJson(Map<String, dynamic> json) {
+    educationYearID = json['Education_year_ID'];
+    schoolsID = json['Schools_ID'];
+    name = json['Name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['Education_year_ID'] = educationYearID;
+    data['Schools_ID'] = schoolsID;
+    data['Name'] = name;
+    return data;
+  }
+}
