25eba6cffd220af381ff4fb1c9640b0b91a0fc0f
abdalghani sabbagh <92229524+abdalghanisabbagh@users.noreply.github.com>
2024-05-27 14:04:09 +0300

    Fix image file names and update seat rendering widget
    - Changed the image file name for the background image from .jpg to .png
    - Updated the image file path in the assets manager
    - Updated the seat rendering widget to display seat numbers on the desks
    - Added a background image to the seat rendering widget
    - Made minor code formatting changes

--- lib/presentation/views/class_room_seats/widgets/render_seat_widget.dart
+++ lib/presentation/views/class_room_seats/widgets/render_seat_widget.dart
@@ -1,43 +1,80 @@
 import 'package:control_system/Data/Models/student_seat/student_seat_model.dart';
 import 'package:control_system/domain/controllers/class_room_controller.dart';
 import 'package:control_system/presentation/resource_manager/assets_manager.dart';
+import 'package:control_system/presentation/resource_manager/color_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/state_manager.dart';
+
+import '../../../resource_manager/styles_manager.dart';
 
 // ignore: must_be_immutable
 class RendarSeats extends StatelessWidget {
   RendarSeats({super.key, required this.seatsNumbers});
   List<StudentSeatModel> seatsNumbers = [];
-  int count = 0;
+  int count = 1;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return GetBuilder<ClassRoomController>(
-      builder: (controller) => controller.classSeats.isEmpty
-          ? const SizedBox.shrink()
-          : ListView.builder(
-              shrinkWrap: true,
-              itemCount: controller.numbers,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      for (int i = 0;
-                          i < controller.classSeats[index];
-                          i++, count++)
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          width: size.width * 0.1,
-                          child:
-                              Image.asset(AssetsManager.assetsImagesClassDesk),
-                        ),
-                    ],
+      builder: (controller) {
+        return controller.classSeats.isEmpty
+            ? const SizedBox.shrink()
+            : Column(
+                children: [
+                  SizedBox(
+                    width: 500,
+                    height: 200,
+                    child: Align(
+                      alignment: AlignmentDirectional.topCenter,
+                      child: Image.asset(
+                        AssetsManager.assetsImagesbackground,
+                        fit: BoxFit.fitWidth,
+                      ),
+                    ),
                   ),
-                );
-              },
-            ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: controller.numbers,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            for (int i = 0;
+                                i < controller.classSeats[index];
+                                i++, count++)
+                              Stack(children: [
+                                Container(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 20),
+                                  width: size.width * 0.1,
+                                  child: Image.asset(
+                                      AssetsManager.assetsImagesClassDesk),
+                                ),
+                                Positioned(
+                                  //       width: 0,
+                                  top: 25,
+                                  //   left: 0,
+                                  right: (size.width * 0.1) / 2 - 10,
+                                  child: Text(
+                                    count.toString(),
+                                    style: nunitoRegular.copyWith(
+                                      color: ColorManager.bgSideMenu,
+                                      fontSize: 35,
+                                    ),
+                                  ),
+                                )
+                              ]),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              );
+      },
     );
   }
 }
