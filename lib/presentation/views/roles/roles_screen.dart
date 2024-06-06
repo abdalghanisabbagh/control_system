d120330c3338e408da32d664e3c21ce633c57c40
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-06-06 09:44:57 +0300

    ✨ Add role screen layout and AddNewRoleWidget component 🚀

--- lib/presentation/views/roles/roles_screen.dart
+++ lib/presentation/views/roles/roles_screen.dart
@@ -1,6 +1,11 @@
+import 'package:control_system/presentation/resource_manager/ReusableWidget/app_dialogs.dart';
 import 'package:flutter/material.dart';
 
+import '../../resource_manager/ReusableWidget/header_widget.dart';
+import '../../resource_manager/color_manager.dart';
+import '../../resource_manager/styles_manager.dart';
 import '../base_screen.dart';
+import 'widgets/add_new_role_widget.dart';
 
 class RolesScreen extends StatelessWidget {
   const RolesScreen({super.key});
@@ -9,8 +14,49 @@ class RolesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BaseScreen(
       key: key,
-      body: const Center(
-        child: Text("Roles Screen"),
+      body: Container(
+        color: ColorManager.bgColor,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const Expanded(
+                  child: HeaderWidget(text: "Roles"),
+                ),
+                InkWell(
+                  onTap: () {
+                    MyDialogs.showDialog(
+                      context,
+                      const AddNewRoleWidget(),
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: ColorManager.glodenColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          "Add New Role",
+                          style: nunitoBold.copyWith(
+                            color: ColorManager.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
