e13f25e83f657356d0eb7e9aab99258773740c68
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-06-12 14:04:14 +0300

    ✨ Add new actions and keys for MyBackButton and PlutoGrid components

--- lib/presentation/resource_manager/ReusableWidget/my_back_button.dart
+++ lib/presentation/resource_manager/ReusableWidget/my_back_button.dart
@@ -1,16 +1,19 @@
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart' show VoidCallback, kIsWeb;
 import 'package:flutter/material.dart'
     show BackButton, BuildContext, StatelessWidget, Widget;
 import 'package:go_router/go_router.dart';
 import 'package:universal_html/html.dart' show window;
 
 class MyBackButton extends StatelessWidget {
-  const MyBackButton({super.key});
+  const MyBackButton({super.key, this.onPressed});
+
+  final VoidCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
     return BackButton(
       onPressed: () {
+        onPressed?.call();
         kIsWeb ? window.history.back() : context.pop();
       },
     );
