edbdb56326d1fe8cfc76616abfc607ff2a58f328
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-06-13 16:49:30 +0300

    ✨ Refactor exam room models and response models 🏫
    - Refactor `ExamRoomResModel` to include additional properties and methods
    - Add new methods to handle converting models to extra data
    - Create `ExamRoomsResModel` for response list handling

--- lib/Data/Models/exam_room/exam_room_res_model.dart
+++ lib/Data/Models/exam_room/exam_room_res_model.dart
@@ -1,20 +1,57 @@
-import 'package:control_system/Data/Models/exam_room/exam_room_model.dart';
-
 class ExamRoomResModel {
-  ExamRoomResModel({this.data});
+  ExamRoomResModel({
+    this.id,
+    this.controlMissionID,
+    this.schoolClassID,
+    this.name,
+    this.stage,
+    this.capacity,
+  });
 
-  ExamRoomResModel.fromJson(json) {
-    data = List<ExamRoomModel>.from(
-        json.map((e) => ExamRoomModel.fromJson(e)).toList());
+  ExamRoomResModel.fromJson(Map<String, dynamic> json) {
+    id = json['ID'];
+    controlMissionID = json['Control_Mission_ID'];
+    schoolClassID = json['School_Class_ID'];
+    name = json['Name'];
+    stage = json['Stage'];
+    capacity = json['Capacity'];
   }
 
-  List<ExamRoomModel>? data;
+  int? capacity;
+  int? id;
+  int? controlMissionID;
+  String? name;
+  int? schoolClassID;
+  String? stage;
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
-    if (this.data != null) {
-      data['data'] = this.data!.map((v) => v.toJson()).toList();
-    }
+    data['ID'] = id;
+    data['Control_Mission_ID'] = controlMissionID;
+    data['School_Class_ID'] = schoolClassID;
+    data['Name'] = name;
+    data['Stage'] = stage;
+    data['Capacity'] = capacity;
     return data;
+  }
+
+  Map<String, String>? toExtra() {
+    return {
+      'ID': id.toString(),
+      'Control_Mission_ID': controlMissionID.toString(),
+      'School_Class_ID': schoolClassID.toString(),
+      'Name': name ?? '',
+      'Stage': stage ?? '',
+      'Capacity': capacity.toString(),
+    };
+  }
+
+  ExamRoomResModel.fromExtra(Map<String, String>? extra) {
+    id = int.parse(extra?['ID'] ?? '0');
+    controlMissionID = int.parse(extra?['Control_Mission_ID'] ?? '0');
+    schoolClassID = int.parse(extra?['School_Class_ID'] ?? '0');
+    name = extra?['Name'];
+    stage = extra?['Stage'];
+    capacity = int.parse(extra?['Capacity'] ?? '0');
   }
 }
