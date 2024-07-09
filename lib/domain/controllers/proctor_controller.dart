09926eb089e6d0129e1ab10d7826fa98d1f16523
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-09 13:08:58 +0300

    ✨ Refactor add proctor form to use MytextFormField (#123)

--- lib/domain/controllers/proctor_controller.dart
+++ lib/domain/controllers/proctor_controller.dart
@@ -1,5 +1,15 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:control_system/domain/controllers/profile_controller.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../../Data/Models/proctor/proctor_res_model.dart';
+import '../../Data/Network/response_handler.dart';
+import '../../Data/Network/tools/failure_model.dart';
+import '../../Data/enums/req_type_enum.dart';
+import '../../app/configurations/app_links.dart';
+import '../../presentation/resource_manager/ReusableWidget/show_dialgue.dart';
 
 class ProctorController extends GetxController {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
@@ -11,6 +21,44 @@ class ProctorController extends GetxController {
   final TextEditingController nisIdController = TextEditingController();
 
   bool showPassord = true;
+
+  bool isLoading = false;
+
+  Future<bool> createNewProctor() async {
+    bool createdSuccessfully = false;
+    isLoading = true;
+    update();
+    ResponseHandler<ProctorResModel> responseHandler = ResponseHandler();
+    Either<Failure, ProctorResModel> response =
+        await responseHandler.getResponse(
+      path: ProctorsLinks.proctor,
+      converter: ProctorResModel.fromJson,
+      type: ReqTypeEnum.POST,
+      body: {
+        "Full_Name": fullNameController.text,
+        "User_Name": usernameController.text,
+        "Password": passwordController.text,
+        "Created_By": Get.find<ProfileController>().cachedUserProfile?.iD,
+      },
+    );
+    isLoading = false;
+    update();
+    response.fold(
+      (l) {
+        MyAwesomeDialogue(
+          title: 'Error',
+          desc: l.message,
+          dialogType: DialogType.error,
+        ).showDialogue(Get.key.currentContext!);
+        createdSuccessfully = false;
+      },
+      (r) {
+        createdSuccessfully = true;
+      },
+    );
+    return createdSuccessfully;
+  }
+
   @override
   void onClose() {
     fullNameController.dispose();
