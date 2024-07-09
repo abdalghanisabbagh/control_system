1481bb46580c14a2c05be01c4aa5a691f3376554
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-09 11:11:34 +0300

    ✨ Add loading indicator reusable widget 🔄
    Added loading indicator reusable widget for better user experience.

---
+++ lib/presentation/resource_manager/ReusableWidget/loading_indicators.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:loading_indicator/loading_indicator.dart';
+
+abstract class LoadingIndicators {
+  static Widget getLoadingIndicator() => const SizedBox(
+        height: 80,
+        width: 80,
+        child: LoadingIndicator(
+          indicatorType: Indicator.pacman,
+        ),
+      );
+}
