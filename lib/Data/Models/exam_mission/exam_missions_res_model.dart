c999cdb6062d8fd21555157287477cf3bc8ebe25
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-10 08:53:34 +0300

    ✨ Refactor exam mission models
    Refactor the exam mission models to separate them into distinct classes for
    better organization and readability.

---
+++ lib/Data/Models/exam_mission/exam_missions_res_model.dart
@@ -0,0 +1,20 @@
+import 'package:control_system/Data/Models/exam_mission/exam_mission_res_model.dart';
+
+class ExamMissionsResModel {
+  ExamMissionsResModel({this.data});
+
+  ExamMissionsResModel.fromJson(json) {
+    data = List<ExamMissionResModel>.from(
+        json.map((e) => ExamMissionResModel.fromJson(e)).toList());
+  }
+
+  List<ExamMissionResModel>? data;
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
