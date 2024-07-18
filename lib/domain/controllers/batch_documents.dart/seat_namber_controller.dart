d4f7380ca88bd8c17e7dee97f8e907f4596badef
abdalghani sabbagh <92229524+abdalghanisabbagh@users.noreply.github.com>
2024-07-18 14:20:57 +0300

    Add SeatNumberController to handle getting education years and control missions
    This commit adds a new file `seat_namber_controller.dart` which contains the `SeatNumberController` class. This controller handles getting education years and control missions based on the selected school. It also includes functions to set the selected education year and get control missions by education year and school. The controller uses `dartz` and `get` packages for state management and `hive_flutter` for local storage. The commit message follows the standard format for Git commit messages.

---
+++ lib/domain/controllers/batch_documents.dart/seat_namber_controller.dart
@@ -0,0 +1,108 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:dartz/dartz.dart';
+import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:multi_dropdown/models/value_item.dart';
+
+import '../../../Data/Models/control_mission/control_missions_res_model.dart';
+import '../../../Data/Models/education_year/educations_years_res_model.dart';
+import '../../../Data/Network/response_handler.dart';
+import '../../../Data/Network/tools/failure_model.dart';
+import '../../../Data/enums/req_type_enum.dart';
+import '../../../app/configurations/app_links.dart';
+import '../../../presentation/resource_manager/ReusableWidget/show_dialgue.dart';
+
+class SeatNumberController extends GetxController {
+  bool isLoadingGetEducationYear = false;
+  bool isLoadingGetControlMission = false;
+  List<ValueItem> optionsEducationYear = [];
+  ValueItem? selectedItemEducationYear;
+  List<ValueItem> optionsControlMission = [];
+  ValueItem? selectedItemControlMission;
+
+
+
+  final int schoolId = Hive.box('School').get('Id');
+
+  @override
+  void onInit() {
+    geteducationyear();
+    super.onInit();
+  }
+
+  Future<void> geteducationyear() async {
+    isLoadingGetEducationYear = true;
+    update();
+    ResponseHandler<EducationsYearsModel> responseHandler = ResponseHandler();
+    Either<Failure, EducationsYearsModel> response =
+        await responseHandler.getResponse(
+      path: EducationYearsLinks.educationyear,
+      converter: EducationsYearsModel.fromJson,
+      type: ReqTypeEnum.GET,
+    );
+    response.fold(
+      (l) {
+        MyAwesomeDialogue(
+          title: 'Error',
+          desc: l.message,
+          dialogType: DialogType.error,
+        ).showDialogue(Get.key.currentContext!);
+      },
+      (r) {
+        List<ValueItem> items = r.data!
+            .map((item) => ValueItem(label: item.name!, value: item.id))
+            .toList();
+        optionsEducationYear = items;
+      },
+    );
+
+    isLoadingGetEducationYear = false;
+    update();
+  }
+
+  void setSelectedItemEducationYear(List<ValueItem> items) {
+    if (items.isNotEmpty) {
+      selectedItemEducationYear = items.first;
+      int educationYearId = selectedItemEducationYear!.value;
+      getControlMissionByEducationYearAndBySchool(educationYearId);
+    } else {
+      selectedItemEducationYear = null;
+    }
+
+    update();
+  }
+
+  Future<void> getControlMissionByEducationYearAndBySchool(
+      int educationYearId) async {
+    isLoadingGetControlMission = true;
+
+    update();
+
+    ResponseHandler<ControlMissionsResModel> responseHandler =
+        ResponseHandler();
+    Either<Failure, ControlMissionsResModel> response =
+        await responseHandler.getResponse(
+      path:
+          "${ControlMissionLinks.controlMissionSchool}/$schoolId/${ControlMissionLinks.controlMissionEducationYear}/$educationYearId",
+      converter: ControlMissionsResModel.fromJson,
+      type: ReqTypeEnum.GET,
+    );
+    response.fold(
+      (l) {
+        MyAwesomeDialogue(
+          title: 'Error',
+          desc: l.message,
+          dialogType: DialogType.error,
+        ).showDialogue(Get.key.currentContext!);
+      },
+      (r) {
+        List<ValueItem> items = r.data!
+            .map((item) => ValueItem(label: item.name!, value: item.iD))
+            .toList();
+        optionsControlMission = items;
+      },
+    );
+    isLoadingGetControlMission = false;
+    update();
+  }
+}
