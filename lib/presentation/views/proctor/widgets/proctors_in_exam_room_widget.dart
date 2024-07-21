d58c21f51190e2fd9b3fe1d72b1f5d28744214d9
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-21 13:19:55 +0300

    🎨 feat(proctors_in_exam_room_widget): Add scrollability to the widget
    The changes in this commit focus on improving the user experience of the `ProctorsInExamRoomWidget`. Previously, the content within the widget was not scrollable, which could lead to a poor experience when the content exceeded the available screen space.
    
    The main change is the addition of a `SingleChildScrollView` widget that wraps the existing `Column` widget. This allows the content to be scrollable, ensuring that the user can access all the information displayed in the widget, even if it exceeds the initial screen size.
    
    This improvement will provide a better overall user experience for the proctors and exam room management functionality within the application.

--- lib/presentation/views/proctor/widgets/proctors_in_exam_room_widget.dart
+++ lib/presentation/views/proctor/widgets/proctors_in_exam_room_widget.dart
@@ -32,114 +32,117 @@ class ProctorsInExamRoomWidget extends GetView<ProctorController> {
                   )
                 : Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Spacer(
-                              flex: 5,
-                            ),
-                            Text(
-                              'Exam Room Name: $examRoomName',
-                              style: nunitoBold,
-                            ),
-                            const Spacer(
-                              flex: 4,
-                            ),
-                            IconButton(
-                              onPressed: () {
-                                Get.back();
-                              },
-                              icon: const Icon(
-                                Icons.close,
+                    child: SingleChildScrollView(
+                      physics: const BouncingScrollPhysics(),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Spacer(
+                                flex: 5,
                               ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              "Session One Exam",
-                              style: nunitoBlack.copyWith(
-                                fontSize: 24,
-                                color: Colors.orange,
+                              Text(
+                                'Exam Room Name: $examRoomName',
+                                style: nunitoBold,
                               ),
-                            ),
-                            const Spacer(),
-                          ],
-                        ),
-                        ListBody(
-                          mainAxis: Axis.vertical,
-                          children: controller.proctorsInExamRoom
-                              .where((proctor) => proctor.period!)
-                              .map(
-                                (proctors) => Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Row(
-                                    children: [
-                                      Text(
-                                        '${proctors.proctor!.userName} ',
-                                      ),
-                                      const Spacer(),
-                                      ElevatedButton(
-                                        onPressed: () async {
-                                          await controller
-                                              .unAssignProctorFromExamRoom(
-                                            proctorId: proctors.id!,
-                                          )
-                                              .then((value) {
-                                            if (value) {
-                                              Get.back();
-                                              MyFlashBar.showSuccess(
-                                                'Unassigned Successfully',
-                                                'Success',
-                                              ).show(Get.key.currentContext!);
-                                            }
-                                          });
-                                        },
-                                        child: const Text('UnAssign'),
-                                      ),
-                                    ],
-                                  ),
+                              const Spacer(
+                                flex: 4,
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  Get.back();
+                                },
+                                icon: const Icon(
+                                  Icons.close,
                                 ),
-                              )
-                              .toList(),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              "Session Two Exams",
-                              style: nunitoBlack.copyWith(
-                                fontSize: 24,
-                                color: Colors.orange,
                               ),
-                            ),
-                            const Spacer(),
-                          ],
-                        ),
-                        ListBody(
-                          mainAxis: Axis.vertical,
-                          children: controller.proctorsInExamRoom
-                              .where((proctor) => !proctor.period!)
-                              .map(
-                                (proctors) => Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    '${proctors.proctor!.userName} ',
-                                  ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "Session One Exam",
+                                style: nunitoBlack.copyWith(
+                                  fontSize: 24,
+                                  color: Colors.orange,
                                 ),
-                              )
-                              .toList(),
-                        ),
-                      ],
+                              ),
+                              const Spacer(),
+                            ],
+                          ),
+                          ListBody(
+                            mainAxis: Axis.vertical,
+                            children: controller.proctorsInExamRoom
+                                .where((proctor) => proctor.period!)
+                                .map(
+                                  (proctors) => Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: [
+                                        Text(
+                                          '${proctors.proctor!.userName} ',
+                                        ),
+                                        const Spacer(),
+                                        ElevatedButton(
+                                          onPressed: () async {
+                                            await controller
+                                                .unAssignProctorFromExamRoom(
+                                              proctorId: proctors.id!,
+                                            )
+                                                .then((value) {
+                                              if (value) {
+                                                Get.back();
+                                                MyFlashBar.showSuccess(
+                                                  'Unassigned Successfully',
+                                                  'Success',
+                                                ).show(Get.key.currentContext!);
+                                              }
+                                            });
+                                          },
+                                          child: const Text('UnAssign'),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                )
+                                .toList(),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "Session Two Exams",
+                                style: nunitoBlack.copyWith(
+                                  fontSize: 24,
+                                  color: Colors.orange,
+                                ),
+                              ),
+                              const Spacer(),
+                            ],
+                          ),
+                          ListBody(
+                            mainAxis: Axis.vertical,
+                            children: controller.proctorsInExamRoom
+                                .where((proctor) => !proctor.period!)
+                                .map(
+                                  (proctors) => Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      '${proctors.proctor!.userName} ',
+                                    ),
+                                  ),
+                                )
+                                .toList(),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
           );
