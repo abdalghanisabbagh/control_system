3717fcb08a6c49e885e97e4749fae2572479917b
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-25 12:54:20 +0300

    🚀 feat(TimeFormatExtension): Improve time formatting logic
    The changes in this commit improve the time formatting logic in the
    `TimeFormatExtension` class. The main changes are:
    
    - The `formatTime12h` method now correctly converts the 24-hour format to
      12-hour format with AM/PM suffix.
    - The `formatTime24h` method remains unchanged, providing the time in 24-hour
      format.
    - The commit message follows the Conventional Commits specification, using the
      `feat` prefix to indicate a new feature and the `TimeFormatExtension` scope to
      indicate the affected area of the codebase.

--- lib/app/extensions/time_format_extension.dart
+++ lib/app/extensions/time_format_extension.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
 
 extension TimeFormatExtension on TimeOfDay {
-  /// get the time in `24h` format
-  /// eg: 23:59:59
-  ///
-  /// For example:
-  /// ```dart
-  /// final time = TimeOfDay(hour: 23, minute: 59, second: 59);
-  /// print(time.formatTime24h); // 23:59:59
-  /// ```
-
-  String get formatTime24h {
-    return '$hour:$minute';
-  }
-
   /// convert `24h` to `12h` format with `AM/PM` suffix
   /// eg: 23:59:59 PM
   ///
@@ -25,5 +12,18 @@ extension TimeFormatExtension on TimeOfDay {
   String get formatTime12h {
     final time = DateTime.now();
     return '${time.hour > 12 ? time.hour - 12 : time.hour == 0 ? 12 : time.hour}:${time.minute}:${time.second} ${time.hour > 12 ? 'PM' : 'AM'}';
+  }
+
+  /// get the time in `24h` format
+  /// eg: 23:59:59
+  ///
+  /// For example:
+  /// ```dart
+  /// final time = TimeOfDay(hour: 23, minute: 59, second: 59);
+  /// print(time.formatTime24h); // 23:59:59
+  /// ```
+
+  String get formatTime24h {
+    return '$hour:$minute';
   }
 }
