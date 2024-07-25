bc0745ac222d8af1732201b16a76d3803de5489e
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-25 12:52:31 +0300

    🐛 fix(roles): fix data field declaration order
    The changes made in this commit fix the declaration order of the `data`
    field in the `RolesResModel` class. The `data` field is now declared at
    the top of the class, followed by the constructor and the `fromJson`
    factory method. This change ensures a more consistent and logical
    structure of the class.

--- lib/Data/Models/user/roles/roleres_model.dart
+++ lib/Data/Models/user/roles/roleres_model.dart
@@ -1,13 +1,13 @@
 import 'role_res_model.dart';
 
 class RolesResModel {
+  List<RoleResModel>? data;
+
   RolesResModel({this.data});
 
   factory RolesResModel.fromJson(json) => RolesResModel(
       data: List<RoleResModel>.from(
           json.map((e) => RoleResModel.fromJson(e)).toList()));
-
-  List<RoleResModel>? data;
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
