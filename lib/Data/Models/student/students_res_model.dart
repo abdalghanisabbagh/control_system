d0d2219499ff0abc68be225bc4227118109aba01
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-25 12:51:07 +0300

    ✨ feat(student): add students property to StudentsResModel
    Adds the `students` property to the `StudentsResModel` class to hold the list
    of `StudentResModel` instances. This change is necessary to improve the
    data structure and make it more intuitive to work with the student
    response data.

--- lib/Data/Models/student/students_res_model.dart
+++ lib/Data/Models/student/students_res_model.dart
@@ -1,14 +1,14 @@
 import 'student_res_model.dart';
 
 class StudentsResModel {
+  List<StudentResModel>? students;
+
   StudentsResModel({this.students});
 
   StudentsResModel.fromJson(json) {
     students = List<StudentResModel>.from(
         json.map((e) => StudentResModel.fromJson(e)).toList());
   }
-
-  List<StudentResModel>? students;
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
