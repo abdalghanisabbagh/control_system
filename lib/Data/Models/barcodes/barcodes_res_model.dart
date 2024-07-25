5927de9e7055805e904e9fc6f9c16bc8342c84c1
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-25 10:28:42 +0300

    🔖 feat(Barcode): add barcode list support
    Adds a new field `barcodes` to the `BarcodesResModel` to support a
    list of barcodes. This change allows the app to handle and display
    multiple barcodes in the UI.

--- lib/Data/Models/barcodes/barcodes_res_model.dart
+++ lib/Data/Models/barcodes/barcodes_res_model.dart
@@ -2,7 +2,9 @@ import 'barcode_res_model.dart';
 
 class BarcodesResModel {
   List<BarcodeResModel>? barcodes;
+
   BarcodesResModel({this.barcodes});
+
   BarcodesResModel.fromJson(json) {
     barcodes = List<BarcodeResModel>.from(
         json.map((e) => BarcodeResModel.fromJson(e)).toList());
