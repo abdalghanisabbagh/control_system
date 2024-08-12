cde7b23e04ac5bdca8efd4474b86c663299f7508
abdalghani sabbagh <92229524+abdalghanisabbagh@users.noreply.github.com>
2024-08-12 12:00:07 +0300

    Change DioFactory base URL to production
    Switch the base URL in DioFactory from the development environment to the production environment by updating the baseUrl parameter in the BaseOptions configuration to use AppLinks.baseUrlProd instead of AppLinks.baseUrlDev. This ensures that network requests made through the Dio instance are directed to the production server, aligning the application's behavior with its intended operational environment.
    
    The change affects the initialization of the Dio instance within the DioFactory class, specifically targeting the configuration of the BaseOptions object, which is used to set the default settings for all requests made through the Dio instance. This update is crucial for maintaining the correct endpoint connectivity during the application's deployment phase.

--- lib/Data/Network/tools/dio_factory.dart
+++ lib/Data/Network/tools/dio_factory.dart
@@ -34,7 +34,7 @@ class DioFactory {
     };
 
     dio.options = BaseOptions(
-      baseUrl: AppLinks.baseUrlDev,
+      baseUrl: AppLinks.baseUrlProd,
       headers: headers,
       receiveTimeout: timeOut,
       sendTimeout: timeOut,
