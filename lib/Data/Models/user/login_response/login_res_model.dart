a89bfea12c251030c3089560415afb73f3c1b5dd
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-20 12:05:12 +0300

    ✨🎉 feat(user): Add user active/deactive functionality
    Adds the ability for admin users to activate and deactivate regular users.
    This allows administrators to easily manage user accounts and control access
    to the system.
    
    The changes include:
    - Adds a `Switch` widget to the `AllUserWidget` to toggle the user's active
      status
    - Updates the `AdminController` to handle the activation/deactivation of users
    
    This feature improves the overall admin experience and provides more
    granular control over user management.

--- lib/Data/Models/user/login_response/login_res_model.dart
+++ lib/Data/Models/user/login_response/login_res_model.dart
@@ -1,4 +1,3 @@
-import '../users_res/user_has_school_res_model.dart';
 import 'user_profile_model.dart';
 
 class LoginResModel {
@@ -7,7 +6,6 @@ class LoginResModel {
   String? refreshToken;
 
   UserProfileModel? userProfile;
-  UserHasSchoolsResModel? userHasSchoolsResModel;
   LoginResModel({
     this.refreshToken,
     this.accessToken,
@@ -19,9 +17,6 @@ class LoginResModel {
     userProfile = json['userProfile'] != null
         ? UserProfileModel.fromJson(json['userProfile'])
         : null;
-    userHasSchoolsResModel = json['userHasSchoolsResModel'] != null
-        ? UserHasSchoolsResModel.fromJson(json['userHasSchoolsResModel'])
-        : null;
   }
 
   Map<String, dynamic> toJson() {
@@ -30,9 +25,6 @@ class LoginResModel {
     data['accessToken'] = accessToken;
     if (userProfile != null) {
       data['userProfile'] = userProfile!.toJson();
-    }
-    if (userHasSchoolsResModel != null) {
-      data['userHasSchoolsResModel'] = userHasSchoolsResModel!.toJson();
     }
     return data;
   }
