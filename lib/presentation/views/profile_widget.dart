b0166500961ecd802da3c501ce117694a6ab6918
abdalghani sabbagh <92229524+abdalghanisabbagh@users.noreply.github.com>
2024-08-20 12:49:09 +0300

    Refactor ProfileWidget to improve readability and maintainability
    - Extracted widget building methods for profile icon, edit profile title, name field, old password field, new password field, confirm new password field, and save button to enhance readability and maintainability.
    - Simplified the build method by calling these extracted methods.
    - Improved the validation logic and error handling for form fields.
    - Updated the context references to use `Get.key.currentContext` for consistency and potential future improvements in context management.

--- lib/presentation/views/profile_widget.dart
+++ lib/presentation/views/profile_widget.dart
@@ -16,8 +16,8 @@ class ProfileWidget extends GetView<ProfileController> {
 
   @override
   Widget build(BuildContext context) {
-    final String? userName =
-        Get.find<ProfileController>().cachedUserProfile?.fullName;
+    final profileController = Get.find<ProfileController>();
+    final String? userName = profileController.cachedUserProfile?.fullName;
     _nameController.text = userName ?? '';
 
     return Scaffold(
@@ -30,136 +30,159 @@ class ProfileWidget extends GetView<ProfileController> {
             fontSize: 20,
           ),
         ),
-        leading: MyBackButton(
-          onPressed: () {
-            Get.back();
-          },
-        ),
+        leading: MyBackButton(onPressed: () => Get.back()),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Center(
-              child: Icon(
-                Icons.account_circle,
-                size: 120,
-                color: ColorManager.greyA8,
-              ),
-            ),
+            _buildProfileIcon(),
             const SizedBox(height: 20),
-            Text(
-              'Edit Profile',
-              style: nunitoBold.copyWith(
-                fontSize: 24,
-                color: ColorManager.black,
-              ),
-            ),
+            _buildEditProfileTitle(),
             const SizedBox(height: 20),
-            TextFormField(
-              controller: _nameController,
-              decoration: const InputDecoration(
-                labelText: 'Name',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.person),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter your name';
-                }
-                return null;
-              },
-            ),
+            _buildNameField(),
             const SizedBox(height: 20),
-            TextFormField(
-              controller: _oldPasswordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Old Password',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.lock),
-              ),
-              validator: (value) {
-                if (_newPasswordController.text.isNotEmpty ||
-                    _confirmNewPasswordController.text.isNotEmpty) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your old password';
-                  }
-                }
-                return null;
-              },
-            ),
+            _buildOldPasswordField(),
             const SizedBox(height: 20),
-            TextFormField(
-              controller: _newPasswordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'New Password',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.lock),
-              ),
-              validator: (value) {
-                if (_confirmNewPasswordController.text.isNotEmpty) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your new password';
-                  }
-                }
-                return null;
-              },
-            ),
+            _buildNewPasswordField(),
             const SizedBox(height: 20),
-            TextFormField(
-              controller: _confirmNewPasswordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Confirm New Password',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.lock),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please confirm your new password';
-                }
-                if (value != _newPasswordController.text) {
-                  return 'Passwords do not match';
-                }
-                return null;
-              },
-            ),
+            _buildConfirmNewPasswordField(),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                final isValid = _validateFields();
-                if (!isValid) return;
-
-                final data = <String, dynamic>{};
-
-                if (_nameController.text.isNotEmpty) {
-                  data['Full_Name'] = _nameController.text;
-                }
-                if (_oldPasswordController.text.isNotEmpty &&
-                    _newPasswordController.text.isNotEmpty) {
-                  data['OldPassword'] = _oldPasswordController.text;
-                  data['NewPassword'] = _newPasswordController.text;
-                }
-
-                final success = await Get.find<ProfileController>().editUser(
-                    data, Get.find<ProfileController>().cachedUserProfile!.iD!);
-
-                if (success) {
-                  Get.back();
-                  MyFlashBar.showSuccess(
-                    'Success',
-                    'Profile updated successfully',
-                  ).show(Get.context!);
-                }
-              },
-              child: const Text('Save Changes'),
-            ),
+            _buildSaveButton(profileController),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildProfileIcon() {
+    return const Center(
+      child: Icon(
+        Icons.account_circle,
+        size: 120,
+        color: ColorManager.greyA8,
+      ),
+    );
+  }
+
+  Widget _buildEditProfileTitle() {
+    return Text(
+      'Edit Profile',
+      style: nunitoBold.copyWith(
+        fontSize: 24,
+        color: ColorManager.black,
+      ),
+    );
+  }
+
+  Widget _buildNameField() {
+    return TextFormField(
+      controller: _nameController,
+      decoration: const InputDecoration(
+        labelText: 'Name',
+        border: OutlineInputBorder(),
+        prefixIcon: Icon(Icons.person),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter your name';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildOldPasswordField() {
+    return TextFormField(
+      controller: _oldPasswordController,
+      obscureText: true,
+      decoration: const InputDecoration(
+        labelText: 'Old Password',
+        border: OutlineInputBorder(),
+        prefixIcon: Icon(Icons.lock),
+      ),
+      validator: (value) {
+        if (_newPasswordController.text.isNotEmpty ||
+            _confirmNewPasswordController.text.isNotEmpty) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter your old password';
+          }
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildNewPasswordField() {
+    return TextFormField(
+      controller: _newPasswordController,
+      obscureText: true,
+      decoration: const InputDecoration(
+        labelText: 'New Password',
+        border: OutlineInputBorder(),
+        prefixIcon: Icon(Icons.lock),
+      ),
+      validator: (value) {
+        if (_confirmNewPasswordController.text.isNotEmpty) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter your new password';
+          }
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildConfirmNewPasswordField() {
+    return TextFormField(
+      controller: _confirmNewPasswordController,
+      obscureText: true,
+      decoration: const InputDecoration(
+        labelText: 'Confirm New Password',
+        border: OutlineInputBorder(),
+        prefixIcon: Icon(Icons.lock),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please confirm your new password';
+        }
+        if (value != _newPasswordController.text) {
+          return 'Passwords do not match';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildSaveButton(ProfileController profileController) {
+    return ElevatedButton(
+      onPressed: () async {
+        if (_validateFields()) {
+          final data = <String, dynamic>{};
+
+          if (_nameController.text.isNotEmpty) {
+            data['Full_Name'] = _nameController.text;
+          }
+          if (_oldPasswordController.text.isNotEmpty &&
+              _newPasswordController.text.isNotEmpty) {
+            data['OldPassword'] = _oldPasswordController.text;
+            data['NewPassword'] = _newPasswordController.text;
+          }
+
+          final success = await profileController.editUser(
+              data, profileController.cachedUserProfile!.iD!);
+
+          if (success) {
+            MyFlashBar.showSuccess(
+              'Success',
+              'Profile updated successfully',
+            ).show(Get.key.currentContext!);
+            Get.back();
+          }
+        }
+      },
+      child: const Text('Save Changes'),
     );
   }
 
@@ -170,12 +193,12 @@ class ProfileWidget extends GetView<ProfileController> {
             _confirmNewPasswordController.text.isEmpty)) {
       MyFlashBar.showError(
               'Validation Error', 'Please fill in at least one field')
-          .show(Get.context!);
+          .show(Get.key.currentContext!);
       return false;
     } else if (_newPasswordController.text !=
         _confirmNewPasswordController.text) {
       MyFlashBar.showError('Validation Error', 'New passwords do not match')
-          .show(Get.context!);
+          .show(Get.key.currentContext!);
       return false;
     }
     return true;
