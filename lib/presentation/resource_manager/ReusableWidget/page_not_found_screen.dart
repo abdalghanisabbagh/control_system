758d39b2ce5af7b2844ee6ac2c09af58bacc8834
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-09-04 11:51:36 +0300

    ✨ feat: Add a PageNotFoundScreen widget
    Adds a new PageNotFoundScreen widget to the
    ReusableWidget directory. This screen will be
    displayed when a user navigates to a non-existent
    page in the application.

---
+++ lib/presentation/resource_manager/ReusableWidget/page_not_found_screen.dart
@@ -0,0 +1,15 @@
+import 'package:custom_theme/lib.dart';
+import 'package:flutter/material.dart';
+
+class PageNotFoundScreen extends StatelessWidget {
+  const PageNotFoundScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Page Not Found" " 404",
+            style: nunitoBold.copyWith(fontSize: 30)),
+      ),
+    );
+  }
+}
