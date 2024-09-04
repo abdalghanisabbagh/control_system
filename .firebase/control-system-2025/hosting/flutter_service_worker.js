fbd8c4b037bf2f295c37563f6fb70268aaeb9682
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-09-04 11:52:26 +0300

    🔥 feat(control-system-2025): Optimize web app performance
    Optimizes the web app's performance by minifying the main.dart.js file in the
    Firebase hosting directory. This change reduces the file size and improves
    the overall loading speed of the web application, enhancing the user
    experience.

--- .firebase/control-system-2025/hosting/flutter_service_worker.js
+++ .firebase/control-system-2025/hosting/flutter_service_worker.js
@@ -79,14 +79,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "b4d2f7a6359c9f92e742823c52c6a593
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "7d844e596d4c0da5e4d0b6b35c196b97",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "70a611d5916ab4bedb596f0abb7f4df2",
+"flutter_bootstrap.js": "d279c66cd06a6cb409034d6bed9d4e25",
 "icons/Icon-192.png": "db9fb010616d67905d18a3c517479f4e",
 "icons/Icon-512.png": "4f1685b6f43d89b05e30955afc5f7aae",
 "icons/Icon-maskable-192.png": "db9fb010616d67905d18a3c517479f4e",
 "icons/Icon-maskable-512.png": "4f1685b6f43d89b05e30955afc5f7aae",
 "index.html": "a63c964448a147502c4910b06bcd20f6",
 "/": "a63c964448a147502c4910b06bcd20f6",
-"main.dart.js": "c2e7a216055c57ae67aeb65b60a49a42",
+"main.dart.js": "d08f89040b76f803f2d195fcb9dd23d2",
 "manifest.json": "31f4ff8120e377f75301a2ee07e32f0e",
 "version.json": "0ddcd5d50591b23980ac47711e07bb49"};
 // The application shell files that are downloaded before a service worker can
