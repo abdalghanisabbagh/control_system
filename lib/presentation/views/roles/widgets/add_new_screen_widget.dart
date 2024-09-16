969a720415d1003f5d54aaa13f2d579d19242860
abdalghani sabbagh <92229524+abdalghanisabbagh@users.noreply.github.com>
2024-09-16 12:53:24 +0300

    Refactor PrivilegesController and AddNewScreenWidget for clarity and validation
    - Renamed variables in PrivilegesController for better readability and consistency.
    - Added a GlobalKey to AddNewScreenWidget for form validation.
    - Implemented form validation in AddNewScreenWidget to ensure all required fields are filled before submitting.
    - Enhanced error handling and feedback in AddNewScreenWidget to provide more informative messages to the user.
    - Updated button actions in AddNewScreenWidget to ensure proper form submission and feedback display.

--- lib/presentation/views/roles/widgets/add_new_screen_widget.dart
+++ lib/presentation/views/roles/widgets/add_new_screen_widget.dart
@@ -12,13 +12,16 @@ import '../../../resource_manager/validations.dart';
 
 class AddNewScreenWidget extends StatelessWidget {
   final TextEditingController nameController = TextEditingController();
-
   final TextEditingController screenIdController = TextEditingController();
+  final GlobalKey<FormState> _formKey =
+      GlobalKey<FormState>(); // مفتاح الـ Form للتحقق من المدخلات
+
   AddNewScreenWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey, // تعيين المفتاح للـ Form
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -47,8 +50,12 @@ class AddNewScreenWidget extends StatelessWidget {
                     )
                   : Row(
                       children: [
-                        const Expanded(
-                          child: ElevatedBackButton(),
+                        Expanded(
+                          child: ElevatedBackButton(
+                            onPressed: () {
+                              Get.back();
+                            },
+                          ),
                         ),
                         const SizedBox(
                           width: 20,
@@ -56,22 +63,33 @@ class AddNewScreenWidget extends StatelessWidget {
                         Expanded(
                           child: ElevatedAddButton(
                             onPressed: () async {
-                              controller
-                                  .addNewScreen(
-                                      name: nameController.text,
-                                      frontId: screenIdController.text)
-                                  .then(
-                                (added) {
-                                  if (added) {
-                                    nameController.clear();
-                                    screenIdController.clear();
-                                    Get.back();
-                                    MyFlashBar.showSuccess(
-                                            'Screen has ben added', 'Screen')
-                                        .show(Get.key.currentContext);
-                                  }
-                                },
-                              );
+                              if (_formKey.currentState!.validate()) {
+                                bool added = await controller.addNewScreen(
+                                  name: nameController.text,
+                                  frontId: screenIdController.text,
+                                );
+
+                                if (added) {
+                                  nameController.clear();
+                                  screenIdController.clear();
+                                  Get.back();
+                                  MyFlashBar.showSuccess(
+                                    'Screen has been added successfully',
+                                    'Success',
+                                  ).show(Get.key.currentContext);
+                                } else {
+                                  MyFlashBar.showError(
+                                    'Failed to add screen',
+                                    'Error',
+                                  ).show(Get.key.currentContext);
+                                }
+                              } else {
+                                // عرض رسالة خطأ في حالة وجود خطأ في الإدخال
+                                MyFlashBar.showError(
+                                  'Please fill in all required fields',
+                                  'Error',
+                                ).show(Get.key.currentContext);
+                              }
                             },
                           ),
                         ),
