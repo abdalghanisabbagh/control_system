9eb62414c5647cf3bbbf12a5cabfa6a65a8e1343
Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-09-29 14:16:40 +0300

    update: update to staging

--- lib/Data/Network/tools/dio_factory.dart
+++ lib/Data/Network/tools/dio_factory.dart
@@ -56,7 +56,7 @@ class DioFactory {
 
     // Set the dio options
     dio.options = BaseOptions(
-      baseUrl: AppLinks.baseUrlProd,
+      baseUrl: AppLinks.baseUrlStaging,
       headers: headers,
 
       // Set the time out for the dio instance
