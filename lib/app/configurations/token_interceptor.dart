20ea71a97f8ed85fbfdabd31c1479f6f8da515fa
Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-10-02 14:35:32 +0300

    deploy: deploy to firebase

--- lib/app/configurations/token_interceptor.dart
+++ lib/app/configurations/token_interceptor.dart
@@ -18,7 +18,7 @@ class TokenInterceptor extends Interceptor {
         String refresh = tokenService.tokenModel!.rToken;
         var dio = Dio(
           BaseOptions(
-            baseUrl: AppLinks.baseUrlStaging,
+            baseUrl: AppLinks.baseUrlProd,
           ),
         );
 
