e395a4fcedb0ad1c4a12f58aa49a26f869b3a027
Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-10-15 10:57:43 +0300

    Update subject_controller.dart

--- lib/domain/controllers/subject/subject_controller.dart
+++ lib/domain/controllers/subject/subject_controller.dart
@@ -24,6 +24,21 @@ class SubjectsController extends GetxController {
   List<int> selectedSchoolTypeIds = <int>[];
   List<SubjectResModel> subjects = <SubjectResModel>[];
 
+  /// Adds a new subject to the server and updates the UI.
+  ///
+  /// The function takes the following parameters:
+  ///
+  /// - [name]: The name of the subject.
+  /// - [inExam]: The value of the inexam field of the subject.
+  /// - [schholTypeIds]: The IDs of the school types that the subject is associated with.
+  ///
+  /// The function will return a boolean indicating whether the subject was added successfully.
+  ///
+  /// The function will also update the UI to show a loading indicator while the request is being processed.
+  ///
+  /// If the response is a failure, the function will show an error dialog with the failure message.
+  ///
+  /// If the response is successful, the function will update the list of subjects in the [SubjectsController] and reset the UI to show that no file has been imported.
   Future<bool> addNewSubject({
     required String name,
     required bool inExam,
@@ -64,6 +79,17 @@ class SubjectsController extends GetxController {
     return subjectHasBeenAdded;
   }
 
+  /// Deletes a subject from the server and updates the UI.
+  ///
+  /// The function takes the ID of the subject to be deleted as a parameter.
+  ///
+  /// The function will show an error dialog if the response is a failure.
+  ///
+  /// The function will also update the UI to show that the subject has been deleted
+  /// from the server.
+  ///
+  /// The function returns a boolean indicating whether the subject was deleted
+  /// successfully.
   Future<bool> deleteSubject({required int id}) async {
     bool subjectHasBeenDeleted = false;
     ResponseHandler<SubjectResModel> responseHandler = ResponseHandler();
@@ -91,10 +117,24 @@ class SubjectsController extends GetxController {
     return subjectHasBeenDeleted;
   }
 
+  /// Edits a subject in the server and updates the UI.
+  //
+  /// The function takes the following parameters:
+  //
+  /// - [id]: The ID of the subject to be edited.
+  /// - [schoolTypeIds]: The IDs of the school types that the subject is associated with.
+  //
+  /// The function will show an error dialog if the response is a failure.
+  //
+  /// The function will also update the UI to show that the subject has been edited
+  /// in the server.
+  //
+  /// The function returns a boolean indicating whether the subject was edited
+  /// successfully.
   Future<bool> editSubject({
     required int id,
     // required String name,
-    required List schholTypeIds,
+    required List schoolTypeIds,
     // required int inExam,
     // required String title,
     // required bool inExam,
@@ -110,7 +150,7 @@ class SubjectsController extends GetxController {
       type: ReqTypeEnum.PATCH,
       body: {
         //"Name": name,
-        'schools_type_ID': schholTypeIds,
+        'schools_type_ID': schoolTypeIds,
         "InExam": 1,
       },
     );
@@ -134,6 +174,16 @@ class SubjectsController extends GetxController {
     return subjectHasBeenUpdated;
   }
 
+  /// Gets all subjects from the server and updates the UI
+  ///
+  /// This function sends a GET request to the server to get all subjects
+  /// for the current school type. The response is handled by the
+  /// [ResponseHandler]. If the response is successful, the subjects list
+  /// is updated and the UI is updated. If the response is not successful,
+  /// an error dialog is shown with the error message from the response.
+  ///
+  /// The function also updates the options for the subject dropdown
+  /// with the subjects from the response.
   Future getAllSubjects() async {
     getAllLoading = true;
     update();
@@ -163,6 +213,15 @@ class SubjectsController extends GetxController {
     );
   }
 
+  /// Gets all the school types from the server and updates the UI
+  ///
+  /// This function sends a GET request to the server to get all school types.
+  /// The response is handled by the [ResponseHandler]. If the response is
+  /// successful, the school types list is updated and the UI is updated. If
+  /// the response is not successful, an error dialog is shown with the error
+  /// message from the response.
+  ///
+  /// The function also updates the loading indicator in the UI.
   Future<void> getSchoolType() async {
     getSchoolTypeLoading = true;
     update();
@@ -190,18 +249,37 @@ class SubjectsController extends GetxController {
   }
 
   @override
+
+  /// Initializes the controller by calling [getSchoolType] and [getAllSubjects] to
+  /// populate the school types and subjects lists respectively. Then calls the
+  /// [onInit] method of the superclass.
   void onInit() {
     getSchoolType();
     getAllSubjects();
     super.onInit();
   }
 
+  /// Updates the [selectedSchoolTypeIds] with the IDs of the selected school types in the
+  /// [selectedOptions] list. Then calls [update] to update the UI.
+  ///
+  /// [selectedOptions] is a list of [ValueItem] objects where the value is the ID of the school type.
+  ///
   void onOptionSelected(List<ValueItem<dynamic>> selectedOptions) {
     selectedSchoolTypeIds =
         selectedOptions.map((e) => e.value).toList().cast<int>();
     update();
   }
 
+  /// Sorts the subjects list by creation time in ascending or descending order.
+  //
+  /// [asc] is a boolean that defaults to true, indicating that the subjects should
+  /// be sorted in ascending order of creation time. If [asc] is false, the subjects
+  /// are sorted in descending order of creation time.
+  //
+  /// The function updates the UI after sorting the list.
+  //
+  /// The function is used in the subject settings page to sort the subjects list
+  /// by creation time.
   void sortSubjectsByCreationTime({bool asc = true}) {
     subjects.sort((a, b) => asc
         ? a.createdAt!.compareTo(b.createdAt!)
@@ -209,6 +287,17 @@ class SubjectsController extends GetxController {
     update();
   }
 
+  /// Sorts the subjects list by name in ascending or descending order, using the same
+  /// logic as [sortCohortsByName].
+  ///
+  /// [asc] is a boolean that defaults to true, indicating that the subjects should
+  /// be sorted in ascending order of name. If [asc] is false, the subjects are sorted
+  /// in descending order of name.
+  ///
+  /// The function updates the UI after sorting the list.
+  ///
+  /// The function is used in the subject settings page to sort the subjects list
+  /// by name.
   void sortSubjectsByName({bool asc = true}) {
     subjects.sort((a, b) {
       final regex = RegExp(r'(\d+)|(\D+)');
