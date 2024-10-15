f49b6052e1cf70f79e1eb24eead78811b2157b26
Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-10-15 10:57:16 +0300

    Update edit_subject_controller.dart

--- lib/domain/controllers/subject/edit_subject_controller.dart
+++ lib/domain/controllers/subject/edit_subject_controller.dart
@@ -23,6 +23,18 @@ class EditSubjectsController extends GetxController {
   List<int> selectedSchoolTypeIds = <int>[];
   List<SubjectResModel> subjects = <SubjectResModel>[];
 
+  /// This function deletes a school type from a subject in the database.
+  ///
+  /// It takes the following parameters:
+  ///
+  /// - [idSubject]: The ID of the subject.
+  /// - [idSchoolType]: The ID of the school type.
+  ///
+  /// The function will return a boolean indicating whether the delete was successful.
+  ///
+  /// The function will also update the UI to show a loading indicator while the request is being processed.
+  ///
+  /// If the response is a failure, the function will show an error dialog with the failure message.
   Future<bool> deleteSchoolTypeInSubject({
     required int idSubject,
     required int idSchoolType,
@@ -58,6 +70,21 @@ class EditSubjectsController extends GetxController {
     return subjectHasBeenUpdated;
   }
 
+  /// Edits a subject in the server and updates the UI.
+  ///
+  /// The function takes the following parameters:
+  ///
+  /// - [id]: The ID of the subject to be edited.
+  /// - [schholTypeIds]: The IDs of the school types that the subject is associated with.
+  /// - [name]: The name of the subject.
+  /// - [inexam]: The value of the inexam field of the subject.
+  /// - [active]: The value of the active field of the subject.
+  ///
+  /// The function will return a boolean indicating whether the subject was edited successfully.
+  ///
+  /// The function will also update the UI to show a loading indicator while the request is being processed.
+  ///
+  /// If the response is a failure, the function will show an error dialog with the failure message.
   Future<bool> editSubject({
     required int id,
     required List schholTypeIds,
@@ -100,6 +127,17 @@ class EditSubjectsController extends GetxController {
     return subjectHasBeenUpdated;
   }
 
+  /// Gets all the school types from the server and updates the UI.
+  ///
+  /// The function sets the [getSchoolTypeLoading] variable to true and then to false
+  /// depending on the response of the API.
+  ///
+  /// If the response is a failure, the function will show an error dialog with the
+  /// failure message.
+  ///
+  /// The function also updates the UI with the schools types returned by the API.
+  ///
+  /// The function is used when the user navigates to the edit subject page.
   Future<void> getSchoolType() async {
     getSchoolTypeLoading = true;
     update();
@@ -127,11 +165,22 @@ class EditSubjectsController extends GetxController {
   }
 
   @override
+
+  /// Called when the widget is initialized.
+  ///
+  /// Calls [getSchoolType] to load the school types and then calls the parent's
+  /// [onInit] method.
   void onInit() {
     getSchoolType();
     super.onInit();
   }
 
+  /// Called when an option is selected from the dropdown.
+  ///
+  /// The function takes the selected options as a parameter and updates
+  /// the [selectedSchoolTypeIds] with the values of the selected options.
+  ///
+  /// The function also calls [update] to update the UI.
   void onOptionSelected(List<ValueItem<dynamic>> selectedOptions) {
     selectedSchoolTypeIds =
         selectedOptions.map((e) => e.value).toList().cast<int>();
