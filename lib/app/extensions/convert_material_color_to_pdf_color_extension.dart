966ac4e381e228907af2611823a1df8629f2eff6
Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-10-15 10:00:23 +0300

    Update convert_material_color_to_pdf_color_extension.dart

--- lib/app/extensions/convert_material_color_to_pdf_color_extension.dart
+++ lib/app/extensions/convert_material_color_to_pdf_color_extension.dart
@@ -3,6 +3,14 @@ import 'dart:ui';
 import 'package:pdf/pdf.dart';
 
 extension ConvertMaterialColorToPdfColorExtension on Color {
+  /// Converts this [Color] to a [PdfColor] using its [value] property.
+  ///
+  /// The [value] property is an integer that represents the color in its
+  /// most compact form. This is the property that is used by the [PdfColor]
+  /// class to represent the color.
+  ///
+  /// This method is useful when you need to convert a [Color] to a [PdfColor]
+  /// in order to use it in a PDF document.
   PdfColor toPdfColorFromValue() {
     return PdfColor.fromInt(value);
   }
