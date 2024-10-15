f1074ba140737f6978b15a64c2bcc60187e367a9
Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-10-15 10:00:42 +0300

    Update app_error_handler.dart

--- lib/Data/Network/tools/app_error_handler.dart
+++ lib/Data/Network/tools/app_error_handler.dart
@@ -3,6 +3,7 @@
 import 'package:control_system/Data/Network/tools/failure_model.dart';
 import 'package:dio/dio.dart';
 
+/// Converts a DioException to a Failure instance.
 Failure _handleError(DioException error) {
   return switch (error.type) {
     DioExceptionType.connectionError =>
@@ -18,6 +19,26 @@ Failure _handleError(DioException error) {
   };
 }
 
+/// An enum that represents the source of the error.
+///
+/// The source of the error can be one of the following:
+///  * [SUCCESS] - the request was successful
+///  * [NO_CONTENT] - the request was successful but there was no content
+///  * [BAD_REQUEST] - the request was invalid or cannot be processed
+///  * [FORBIDDEN] - the request was forbidden or the user is not authorized
+///  * [UNAUTORISED] - the request was unauthorized
+///  * [NOT_FOUND] - the request was not found
+///  * [INTERNAL_SERVER_ERROR] - the server encountered an unexpected condition
+///  * [CONNECT_TIMEOUT] - the request timed out
+///  * [CANCEL] - the request was cancelled
+///  * [RECIEVE_TIMEOUT] - the request timed out
+///  * [SEND_TIMEOUT] - the request timed out
+///  * [CACHE_ERROR] - the request was cache related
+///  * [NO_INTERNET_CONNECTION] - the device has no internet connection
+///  * [DEFAULT] - the request has a default error
+///  * [CONNECTION_ERROR] - the request had a connection error
+///  * [BAD_CERTIFICATE] - the request had a bad certificate
+///  * [BAD_RESPONSE] - the request had a bad response
 enum DataSource {
   SUCCESS,
   NO_CONTENT,
@@ -38,79 +59,187 @@ enum DataSource {
   BAD_RESPONSE,
 }
 
+/// A class that handles errors and converts them to a [Failure] instance.
+///
+/// The [ErrorHandler] class takes a dynamic error and checks if it is a
+/// [DioException]. If it is, it calls the [_handleError] function to convert
+/// the [DioException] to a [Failure] instance. If not, it sets the [failure]
+/// property to a default [Failure] instance with a code of [ResponseCode.DEFAULT]
+/// and a message of 'DEFAULT'.
 class ErrorHandler implements Exception {
+  /// The failure instance that will be returned.
   late Failure failure;
 
-  ErrorHandler.handle(dynamic error) {
+  /// A factory method that takes a dynamic error and returns a [Failure] instance.
+  ///
+  /// If the error is a [DioException], it calls the [_handleError] function to
+  /// convert the [DioException] to a [Failure] instance. If not, it sets the
+  /// [failure] property to a default [Failure] instance with a code of
+  /// [ResponseCode.DEFAULT] and a message of 'DEFAULT'.
+  factory ErrorHandler.handle(dynamic error) {
+    ErrorHandler errorHandler = ErrorHandler._internal();
+
     if (error is DioException) {
       // dio error so its an error from response of the API or from dio itself
-      failure = _handleError(error);
+      errorHandler.failure = _handleError(error);
     } else {
       // default error
-      failure = DataSource.DEFAULT.getFailure();
+      errorHandler.failure = DataSource.DEFAULT.getFailure();
     }
+
+    return errorHandler;
   }
+
+  /// A private constructor that initializes the [ErrorHandler] class.
+  ErrorHandler._internal();
 }
 
+/// Codes for response status.
 class ResponseCode {
-  static const int BAD_CERTIFICATE = 600; // failure, bad certificate
-  static const int BAD_REQUEST = 400; // failure, API rejected request
-  static const int BAD_RESPONSE = -8; // failure, bad response
+  /// Bad certificate.
+  /// Failure, bad certificate.
+  static const int BAD_CERTIFICATE = 600;
+
+  /// Bad request.
+  /// Failure, API rejected request.
+  static const int BAD_REQUEST = 400;
+
+  /// Bad response.
+  /// Failure, bad response.
+  static const int BAD_RESPONSE = -8;
+
+  /// Cache error.
+  /// Failure, cache error.
   static const int CACHE_ERROR = -5;
+
+  /// Cancel.
+  /// Failure, request was cancelled.
   static const int CANCEL = -2;
+
+  /// Connect timeout.
+  /// Failure, could not connect to the server.
   static const int CONNECT_TIMEOUT = -1;
-  // local status code
+
+  /// Default response code.
+  /// Success, no data.
   static const int DEFAULT = 0000;
 
-  static const int FORBIDDEN = 403; //  failure, API rejected request
-  static const int INTERNAL_SERVER_ERROR = 500; // failure, crash in server side
-  static const int NOT_FOUND = 404; // failure, not found
-  static const int NO_CONTENT = 201; // success with no data (no content)
+  /// Forbidden.
+  /// Failure, API rejected request.
+  static const int FORBIDDEN = 403;
+
+  /// Internal server error.
+  /// Failure, crash in server side.
+  static const int INTERNAL_SERVER_ERROR = 500;
+
+  /// Not found.
+  /// Failure, not found.
+  static const int NOT_FOUND = 404;
+
+  /// No content.
+  /// Success with no data (no content).
+  static const int NO_CONTENT = 201;
+
+  /// No internet connection.
+  /// Failure, no internet connection.
   static const int NO_INTERNET_CONNECTION = -6;
+
+  /// Receive timeout.
+  /// Failure, receive timeout.
   static const int RECIEVE_TIMEOUT = -3;
+
+  /// Send timeout.
+  /// Failure, send timeout.
   static const int SEND_TIMEOUT = -4;
-  static const int SUCCESS = 200; // success with data
-  static const int UNAUTORISED = 401; // failure, user is not authorised
+
+  /// Success.
+  /// Success with data.
+  static const int SUCCESS = 200;
+
+  /// Unauthorised.
+  /// Failure, user is not authorised.
+  static const int UNAUTORISED = 401;
 }
 
+/// Class containing all the error messages from the API.
 class ResponseMessage {
+  /// Error message for a bad certificate.
   static const String BAD_CERTIFICATE = "Bad certificate";
-  static const String BAD_REQUEST =
-      "Bad request, Try again later"; // failure, API rejected request
 
+  /// Error message for a bad request.
+  /// Failure, API rejected request.
+  static const String BAD_REQUEST = "Bad request, Try again later";
+
+  /// Error message for a bad response.
   static const String BAD_RESPONSE = "Bad Response";
+
+  /// Error message for a cache error.
+  /// Failure, cache related.
   static const String CACHE_ERROR = "Cache error, Try again later";
+
+  /// Error message for a cancelled request.
+  /// Failure, request was cancelled.
   static const String CANCEL = "Request was cancelled, Try again later";
+
+  /// Error message for a connection error.
+  /// Failure, connection related.
   static const String CONNECTION_ERROR =
       "Some thing went wrong, Try again later";
 
   // local status code
+  /// Error message for a connect timeout.
+  /// Failure, request timed out.
   static const String CONNECT_TIMEOUT = "Time out error, Try again later";
 
+  /// Default error message.
+  /// Failure, unknown error.
   static const String DEFAULT = "Some thing went wrong, Try again later";
-  static const String FORBIDDEN =
-      "Forbidden request, Try again later"; //  failure, API rejected request
 
+  /// Error message for a forbidden request.
+  /// Failure, API rejected request.
+  static const String FORBIDDEN = "Forbidden request, Try again later";
+
+  /// Error message for an internal server error.
+  /// Failure, crash in server side.
   static const String INTERNAL_SERVER_ERROR =
-      "Some thing went wrong, Try again later"; // failure, crash in server side
+      "Some thing went wrong, Try again later";
 
-  static const String NOT_FOUND =
-      "Some thing went wrong, Try again later"; // failure, crash in server side
+  /// Error message for a not found error.
+  /// Failure, not found.
+  static const String NOT_FOUND = "Some thing went wrong, Try again later";
 
-  static const String NO_CONTENT =
-      "success"; // success with no data (no content)
+  /// Error message for a no content error.
+  /// Success with no data (no content).
+  static const String NO_CONTENT = "success";
 
+  /// Error message for a no internet connection error.
+  /// Failure, no internet connection.
   static const String NO_INTERNET_CONNECTION =
       "Please check your internet connection";
 
+  /// Error message for a receive timeout.
+  /// Failure, request timed out.
   static const String RECIEVE_TIMEOUT = "Time out error, Try again later";
+
+  /// Error message for a send timeout.
+  /// Failure, request timed out.
   static const String SEND_TIMEOUT = "Time out error, Try again later";
-  static const String SUCCESS = "success"; // success with data
-  static const String UNAUTORISED =
-      "User is unauthorised, Try again later"; // failure, user is not authorised
+
+  /// Error message for a success error.
+  /// Success with data.
+  static const String SUCCESS = "success";
+
+  /// Error message for an unauthorised error.
+  /// Failure, user is not authorised.
+  static const String UNAUTORISED = "User is unauthorised, Try again later";
 }
 
 extension DataSourceExtension on DataSource {
+  /// Returns a [Failure] instance based on the [DataSource] enum value.
+  ///
+  /// The returned [Failure] instance will have a code and message based on the
+  /// [DataSource] enum value.
+  ///
   Failure getFailure() {
     return switch (this) {
       DataSource.SUCCESS =>
