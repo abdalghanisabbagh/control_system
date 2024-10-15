ff3c94a3c1cc93917aff48e846bad9b6186367fb
Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-10-15 10:56:25 +0300

    Update cohorts_settings_controller.dart

--- lib/domain/controllers/cohorts_settings_controller.dart
+++ lib/domain/controllers/cohorts_settings_controller.dart
@@ -19,9 +19,20 @@ class CohortsSettingsController extends GetxController {
   bool deleteSubjectLoading = false;
   bool getAllLoading = false;
   List<int> selectedSubjectsIds = <int>[];
-  List<int> slectedSchoolTypeId = <int>[];
+  List<int> selectedSchoolTypeId = <int>[];
 
-  Future<bool> addnewCohort(String name) async {
+  /// Adds a new cohort to the server and updates the UI.
+  ///
+  /// The function takes the name of the cohort as a parameter.
+  ///
+  /// The function will show an error dialog if the response is a failure.
+  ///
+  /// The function will also update the UI to show that the cohort has been added
+  /// to the server.
+  ///
+  /// The function returns a boolean indicating whether the cohort was added
+  /// successfully.
+  Future<bool> addNewCohort(String name) async {
     addLoading = true;
     update();
     bool cohortHasBeenAdded = false;
@@ -33,7 +44,7 @@ class CohortsSettingsController extends GetxController {
       type: ReqTypeEnum.POST,
       body: {
         "Name": name,
-        "School_Type_ID": slectedSchoolTypeId.firstOrNull ??
+        "School_Type_ID": selectedSchoolTypeId.firstOrNull ??
             Hive.box('School').get('SchoolTypeID'),
       },
     );
@@ -59,6 +70,21 @@ class CohortsSettingsController extends GetxController {
     return cohortHasBeenAdded;
   }
 
+  /// Adds the selected subjects to the cohort with the given [id].
+  ///
+  /// The function first sets [addLoading] to true and calls [update].
+  ///
+  /// Then, it makes a POST request to the server to connect the selected
+  /// subjects to the cohort.
+  ///
+  /// If the response is a failure, it shows an error dialog with the failure
+  /// message.
+  ///
+  /// If the response is a success, it sets [addLoading] to false, calls [update]
+  /// and clears [selectedSubjectsIds].
+  ///
+  /// The function returns a boolean indicating whether the subjects were added
+  /// successfully.
   Future<bool> addNewSubjectsToCohort(int id) async {
     addLoading = true;
     bool cohortHasBeenAdded = false;
@@ -92,6 +118,19 @@ class CohortsSettingsController extends GetxController {
     return cohortHasBeenAdded;
   }
 
+  /// Deletes a cohort from the server and updates the UI.
+  ///
+  /// The function makes a DELETE request to the server to delete the cohort
+  /// with the given [id].
+  ///
+  /// If the response is a failure, it shows an error dialog with the failure
+  /// message.
+  ///
+  /// If the response is a success, it calls [getAllCohorts] and updates the UI
+  /// to show that the cohort has been deleted from the server.
+  ///
+  /// The function returns a boolean indicating whether the cohort was deleted
+  /// successfully.
   Future<bool> deleteCohort(int id) async {
     bool cohortHasBeenDeleted = false;
     ResponseHandler<void> responseHandler = ResponseHandler();
@@ -120,6 +159,21 @@ class CohortsSettingsController extends GetxController {
     return cohortHasBeenDeleted;
   }
 
+  /// Deletes a subject from a cohort and updates the UI.
+  ///
+  /// The function takes two parameters: [cohortId] and [subjectId].
+  ///
+  /// It makes a POST request to the server to delete the subject with the given
+  /// [subjectId] from the cohort with the given [cohortId].
+  ///
+  /// If the response is a failure, it shows an error dialog with the failure
+  /// message.
+  ///
+  /// If the response is a success, it calls [getAllCohorts] and updates the UI
+  /// to show that the subject has been deleted from the cohort.
+  ///
+  /// The function returns a boolean indicating whether the subject was deleted
+  /// successfully.
   Future<bool> deleteSubjectFromCohort({
     required int cohortId,
     required int subjectId,
@@ -153,6 +207,21 @@ class CohortsSettingsController extends GetxController {
     return subjectHasBeenDeleted;
   }
 
+  /// Edits a cohort in the server and updates the UI.
+  ///
+  /// The function takes three parameters: [id], [name], and [schoolTypeID].
+  ///
+  /// It makes a PATCH request to the server to update the cohort with the given
+  /// [id] with the given [name] and [schoolTypeID].
+  ///
+  /// If the response is a failure, it shows an error dialog with the failure
+  /// message.
+  ///
+  /// If the response is a success, it calls [getAllCohorts] and updates the UI
+  /// to show that the cohort has been updated.
+  ///
+  /// The function returns a boolean indicating whether the cohort was updated
+  /// successfully.
   Future<bool> editCohort(int id, String name, int? schoolTypeID) async {
     addLoading = true;
     update();
@@ -191,6 +260,19 @@ class CohortsSettingsController extends GetxController {
     return cohortHasBeenAdded;
   }
 
+  /// Gets all cohorts from the server and updates the UI.
+  ///
+  /// The function sends a GET request to the server to get all cohorts
+  /// for the current school type. The response is handled by the
+  /// [ResponseHandler].
+  ///
+  /// If the response is a failure, it shows an error dialog with the failure
+  /// message.
+  ///
+  /// If the response is a success, it updates the [cohorts] list and updates
+  /// the UI to show the new list of cohorts.
+  ///
+  /// The function returns a Future of void.
   Future getAllCohorts() async {
     getAllLoading = true;
     update();
@@ -215,17 +297,52 @@ class CohortsSettingsController extends GetxController {
   }
 
   @override
+
+  /// This function is called when the widget is initialized.
+  ///
+  /// It calls [getAllCohorts] to get all cohorts from the server and
+  /// then calls the [onInit] function of the superclass to complete the
+  /// initialization process.
   void onInit() {
     getAllCohorts();
     super.onInit();
   }
 
+  /// Updates the [selectedSubjectsIds] with the IDs of the selected subjects in the
+  /// [selectedOptions] list. Then calls [update] to update the UI.
+  ///
+  /// [selectedOptions] is a list of [ValueItem] objects where the value is the ID of the subject.
   void onOptionSelected(List<ValueItem<dynamic>> selectedOptions) {
     selectedSubjectsIds =
         selectedOptions.map((e) => e.value as int).toList().cast<int>();
     update();
   }
 
+  /// Sorts the [cohorts] list by the creation time of the cohorts.
+  ///
+  /// The sorting is done in ascending order by default, but can be changed
+  /// to descending order by setting [asc] to false.
+  ///
+  /// After sorting, the UI is updated by calling [update].
+  ///
+  void sortCohortsByCreationTime({bool asc = true}) {
+    cohorts.sort((a, b) => asc
+        ? a.createdAt!.compareTo(b.createdAt!)
+        : b.createdAt!.compareTo(a.createdAt!));
+    update();
+  }
+
+  /// Sorts the [cohorts] list by name in ascending or descending order, using the same
+  /// logic as [sortCohortsByCreationTime].
+  ///
+  /// [asc] is a boolean that defaults to true, indicating that the cohorts should
+  /// be sorted in ascending order of name. If [asc] is false, the cohorts are sorted
+  /// in descending order of name.
+  ///
+  /// The function updates the UI after sorting the list.
+  ///
+  /// The function is used in the cohort settings page to sort the cohorts list
+  /// by name.
   void sortCohortsByName({bool asc = true}) {
     cohorts.sort((a, b) {
       final regex = RegExp(r'(\d+)|(\D+)');
@@ -252,13 +369,6 @@ class CohortsSettingsController extends GetxController {
 
       return asc ? a.name!.compareTo(b.name!) : b.name!.compareTo(a.name!);
     });
-    update();
-  }
-
-  void sortCohortsByCreationTime({bool asc = true}) {
-    cohorts.sort((a, b) => asc
-        ? a.createdAt!.compareTo(b.createdAt!)
-        : b.createdAt!.compareTo(a.createdAt!));
     update();
   }
 }
