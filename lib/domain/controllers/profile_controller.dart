d2756bb7975e604bff594364635c0df78531c0b3
Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-10-15 10:57:00 +0300

    Update profile_controller.dart

--- lib/domain/controllers/profile_controller.dart
+++ lib/domain/controllers/profile_controller.dart
@@ -20,6 +20,14 @@ class ProfileController extends GetxController {
   UserProfileModel? get cachedUserProfile =>
       _cachedUserProfile ?? getProfileFromHiveBox();
 
+  /// Check if the user has access to the given widgetId by checking if the
+  /// role the user belongs to has a screen with the given widgetId.
+  ///
+  /// This method will first get the roles of the user using [cachedUserProfile].
+  /// Then it will map each role to a list of strings which are the frontId of
+  /// the screens the role has. Then it will check if the given widgetId is
+  /// present in any of the lists. If it is, it will return true, otherwise it
+  /// will return false.
   bool canAccessWidget({required String widgetId}) {
     return (cachedUserProfile?.roles
             ?.map((role) => role.screens
@@ -30,11 +38,29 @@ class ProfileController extends GetxController {
         false);
   }
 
+  /// Deletes the user profile from the Hive box and clears the cached user profile.
+  ///
   Future<void> deleteProfileFromHiveBox() async {
     _cachedUserProfile = null;
     await Hive.box('Profile').clear();
   }
 
+  /// Edits a user with the given [id] and [data] and returns a boolean indicating
+  /// whether the operation was successful.
+  ///
+  /// The function takes two parameters: [data] and [id]. [data] is the data to be
+  /// updated in the user and [id] is the ID of the user to be updated.
+  ///
+  /// The function will return a boolean indicating whether the operation was
+  /// successful. If the operation is successful, the function will return true,
+  /// otherwise it will return false.
+  ///
+  /// The function will also show a loading indicator while the request is being
+  /// processed.
+  ///
+  /// If the response is a failure, the function will show an error dialog with the
+  /// failure message.
+  ///
   Future<bool> editUser(Map<String, dynamic> data, int id) async {
     isLoadingEditUser = true;
     update();
@@ -67,6 +93,17 @@ class ProfileController extends GetxController {
     );
   }
 
+  /// Gets the user profile from the Hive box and returns it as a
+  /// [UserProfileModel].
+  ///
+  /// If the user profile is not present in the Hive box, the method will return
+  /// null.
+  ///
+  /// The method will first get the data from the Hive box using the key 'Profile'.
+  /// If the key is present in the box, the method will decode the data and
+  /// convert it to a [UserProfileModel] object. If the key is not present, the
+  /// method will return null.
+  ///
   UserProfileModel? getProfileFromHiveBox() {
     var data = Hive.box('Profile').get('Profile');
     _cachedUserProfile = Hive.box('Profile').containsKey("Profile")
@@ -75,6 +112,14 @@ class ProfileController extends GetxController {
     return _cachedUserProfile;
   }
 
+  /// Saves the user profile to the Hive box and updates the cached user profile.
+  ///
+  /// The function takes a [UserProfileModel] object as a parameter and saves it
+  /// to the Hive box using the key 'Profile'. The function will also update the
+  /// cached user profile and notify the UI to rebuild using the [update] method.
+  ///
+  /// The function will not return anything.
+  ///
   void saveProfileToHiveBox(UserProfileModel cachedUserProfile) {
     _cachedUserProfile = cachedUserProfile;
     update();
